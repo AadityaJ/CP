f0370dfcd01ee4b7e2bd81de9ff88b7d80e599ec
AadityaJ <aadityajamuar@gmail.com>
2016-01-16 00:40:50 +0530

    Adding

--- Jan2016/edist.cpp
+++ Jan2016/edist.cpp
@@ -1,2 +1,12 @@
 //http://www.spoj.com/problems/EDIST/
 //longes common subsequence
+#include <stdio.h>
+using namespace std;
+int lcs(int a,int b){
+	
+}
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
