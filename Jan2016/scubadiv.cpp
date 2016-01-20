cea4dd871ca855e4dca2c61f3cf8916bb6208d8c
AadityaJ <aadityajamuar@gmail.com>
2016-01-20 08:52:52 +0530

    adding

--- Jan2016/scubadiv.cpp
+++ Jan2016/scubadiv.cpp
@@ -3,10 +3,21 @@
 int max(int a,int b){
 	return (a<b)?b:a;
 }
+int dp[25][80][1001];
+void init(){
+	for(int i=0;i<25;i++){
+		for(int j=0;j<80;j++){
+			for(int k=0;k<1001;k++)
+				dp[i][j][k]=-1;
+		}
+	}
+}
 int knp(int wo,int wn,int wt[][2],int val[],int n){
-	if(n==0 || (wo==0 && wn==0)) return 0;
-	if(wo<wt[n-1][0] && wn<wt[n-1][1]) return knp(wo,wn,wt,val,n-1);
-	else return max(val[n-1]+knp(wo-wt[n-1][0],wn-wt[n-1][1],wt,val,n-1),knp(wo,wn,wt,val,n-1));
+	if(dp[wo][wn][n]!=-1) return dp[wo][wn][n]; 
+	if(n==0 || (wo<=0 && wn<=0)) dp[wo][wn][n]=0;
+	if(wo<wt[n-1][0] && wn<wt[n-1][1]) dp[wo][wn][n]=knp(wo,wn,wt,val,n-1);
+	else dp[wo][wn][n]=max(val[n-1]+knp(wo-wt[n-1][0],wn-wt[n-1][1],wt,val,n-1),knp(wo,wn,wt,val,n-1));
+	return dp[wo][wn][n];
 }
 int main(int argc, char const *argv[])
 {
