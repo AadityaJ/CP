dea57925ebcffcf6d461e128d6e01e212eba0631
AadityaJ <aadityajamuar@gmail.com>
2016-01-24 01:01:53 +0530

    adding

--- Jan2016/mtplilot.c
+++ Jan2016/mtplilot.c
@@ -2,17 +2,19 @@
 #include <stdio.h>
 int arr[10001][2];
 int n;
-int pil=0,asn=0;
-int max(int a,int b){
-	return (a>b)?a:b;
+//int pil=0,ass=0;
+int min(int a,int b){
+	return (a<b)?a:b;
 }
-int calc(int i,int a){
-	if(i==n) {
-		if(pil==(asn/2))return 0;
-		else return -10000;}
-	if(a==0) pil++;
-	else asn++;
-	return arr[i][a]+max(arr[i+1][a],arr[i+1][!a]);
+int calc(int i,int a,int pil,int asn){
+	//if(i==n) {return 0;}
+	if(i==n){
+		if(asn==pil) return 0;
+		else return -1000;
+	}
+	if(a==0) return arr[i][a]+min(calc(i+1,a,pil+1,asn),calc(i+1,!a,pil,asn)) ;
+	else asn ++; 
+	
 }
 int main(int argc, char const *argv[])
 {	
@@ -20,7 +22,7 @@ int main(int argc, char const *argv[])
 	for(int i=0;i<n;i++){
 		scanf("%d %d",&arr[i][0],&arr[i][1]);
 	}
-	int res=max(calc(0,0),calc(0,1));
+	int res=min(calc(0,0,0,0),calc(0,1,0,0));
 	printf("%d\n",res);
 	return 0;
 }
