15b1ad98936f572a3bc1691b26160c464b281f9b
AadityaJ <aadityajamuar@gmail.com>
2016-01-23 19:44:31 +0530

    adding changes

--- Jan2016/addmax.c
+++ Jan2016/addmax.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int n;
-int iter;
+//int iter;
 int dp[100][100];
 void init(int n){
 	for(int i=0;i<n;i++){
@@ -8,12 +8,12 @@ void init(int n){
 	}
 }
 int calc(int r,int c){
-	iter++;
+	//iter++;
 	if(dp[r][c]!=-1) return dp[r][c];
 	if(r<0 || r>=n) return 0;
 	if(c<0 || c>=n) return 0;
 	if(c==n-1 && r==n-1) return 1;
-	return dp[r][c]=calc(r+1,c)+calc(r,c+1);
+	return dp[r][c]=calc(r+1,c)+calc(r,c+1)+calc(r+1,c+1);//+calc(r-1,c)+calc(r,c-1);
 }
 int main(int argc, char const *argv[])
 {	int t;
