d0f80e935a8f3ae4488143b788b8cb32fc959fa0
AadityaJ <aadityajamuar@gmail.com>
2016-01-26 12:04:19 +0530

    adding

--- Jan2016/temptisl.c
+++ Jan2016/temptisl.c
@@ -9,15 +9,17 @@ void init(){
 			dp[i][j]=-100;
 	}
 }
+//int iter=0;
 int comp(int a,int k){
 	if(dp[a][k]!=-100) return dp[a][k];
+	//iter++;
 	if(k==0){
 		if(a==b) return 1;
 		else return 0;
 	}
 	//printf("%d %d %d\n",b,a,k);
-	if(a==1) return dp[arr[n]][k-1]+dp[arr[2]][k-1];	
-	return dp[a][k]=dp[arr[(a-1)%n]][k-1]+dp[arr[(a+1)%n]][k-1];
+	if(a==1) return comp(arr[n],k-1)+comp(arr[2],k-1);	
+	return dp[a][k]=comp(arr[(a-1)%n],k-1)+comp(arr[(a+1)%n],k-1);
 }
 int main(int argc, char const *argv[])
 {
@@ -29,6 +31,7 @@ int main(int argc, char const *argv[])
 		if(n==(-1) && k==-1) break;	
 		scanf("%d %d",&a,&b);
 		printf("%d\n",comp(a,k));
+		//printf("%d\n",iter);
 	}
 	return 0;
 }
