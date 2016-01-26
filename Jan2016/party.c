67477339f6f76a4702989f46ff90b1c4716f0351
AadityaJ <aadityajamuar@gmail.com>
2016-01-26 23:38:20 +0530

    adding and updating

--- Jan2016/party.c
+++ Jan2016/party.c
@@ -7,13 +7,24 @@ int arr[105][105];
 int max(int a,int b){
 	return (a>b)?a:b;
 }
+int min(int a,int b){
+	return (a>b)?b:a;
+}
+int numfun;
 int dp(int i,int bound,int fun){
 	if(i==n){
-		if(bound<=budget){printf("%d \n",bound); return fun;}
+		if(bound<=budget)return fun;
 		else return 0;
 	}
 	return max(dp(i+1,bound+arr[i][0],fun+arr[i][1]),dp(i+1,bound,fun));
 } 
+int ex(int i,int bound,int fun){
+	if(i==n){
+		if(fun==numfun) return bound;
+		else return 0;
+	}
+	return max(dp(i+1,bound+arr[i][0],fun+arr[i][1]),dp(i+1,bound,fun));
+}
 /*
 	- Add memoisation to reduce iterations
 	- How to return what is the "expenditure"
@@ -24,7 +35,9 @@ int main(int argc, char const *argv[])
 		scanf("%d %d",&budget,&n);
 		if(!budget && !n) break;
 		for(int i=0;i<n;i++) scanf("%d %d",&arr[i][0],&arr[i][1]);
-		printf("%d %d\n",dp(0,0,0),exp);
+		numfun=dp(0,0,0);
+		exp=ex(0,0,0);
+		printf("%d %d\n",numfun,exp);
 	}
 	return 0;
 }
