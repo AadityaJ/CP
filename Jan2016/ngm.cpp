fe5ff0083eddd9c8a9a8e8a6c6b10a3d976fdd8b
AadityaJ <aadityajamuar@gmail.com>
2016-01-26 23:51:05 +0530

    adding

---
+++ Jan2016/ngm.cpp
@@ -0,0 +1,17 @@
+//http://www.spoj.com/problems/NGM/
+#include <iostream>
+using namespace std;
+
+#define MAX_NUM 2000000000
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n % 10 == 0)
+        cout << "2";
+    else {
+        cout << "1" << endl;
+        cout << (n%10) << endl;
+    }
+}
