da39039ad83225270f2f21fe04958101b38fc689
AadityaJ <aadityajamuar@gmail.com>
2016-01-31 21:21:17 +0530

    adding

--- Jan2016/samer08f.c
+++ Jan2016/samer08f.c
@@ -1,15 +1,11 @@
 //http://www.spoj.com/problems/SAMER08F/
-#include <cstdio>
-
-using namespace std;
-
-int main(){
-    int n;
-    
+#include <stdio.h>
+int main(int argc, char const *argv[]){
+    int n;  
     while(1){
         scanf("%d",&n);
         if(n==0) break;
-        
         printf("%d\n",n*(n+1)/2*(2*n+1)/3);
-    }
+        }
+	return 0;
 }
