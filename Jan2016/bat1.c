109a8e3ba574bcfd548189853104ed9ceea145e7
AadityaJ <aadityajamuar@gmail.com>
2016-01-31 21:15:43 +0530

    adding

--- Jan2016/bat1.c
+++
@@ -1,9 +0,0 @@
-//http://www.spoj.com/problems/BAT1/
-//difficult to map using greedy and will thus try dp
-//variation of knapsack
-#include <stdio.h>
-int main(int argc, char const *argv[])
-{
-	
-	return 0;
-}
