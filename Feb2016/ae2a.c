81227f28fce63f04373f8d8f2ba5794290812cb6
AadityaJ <aadityajamuar@gmail.com>
2016-02-05 16:42:08 +0530

    adding

--- Feb2016/ae2a.c
+++ Feb2016/ae2a.c
@@ -1,25 +1,28 @@
 //http://www.spoj.com/problems/AE2A/
 #include <stdio.h>
-#define max 1e+6;
+#define max 1000006;
 long long int n,k;
-int dp[max][max];
+int dp[1000006][1000006];
 void init(){
-	for(int i=0;i<max;i++){
-		for(int j=0;j<max;j++) dp[i][j]=0;
+	for(int i=0;i<1000006;i++){
+		for(int j=0;j<1000006;j++) 
+			dp[i][j]=-100;
 	}
 }
 int calc(long long int i,long long int pip){
+	if(dp[i][pip]!=-100) return dp[i][pip];
 	if(i==n){
 		if(pip==k) return 1;
 		else return 0;
 	}
-	return calc(i+1,pip+1)+calc(i+1,pip+2)+calc(i+1,pip+3)+calc(i+1,pip+4)+calc(i+1,pip+5)+calc(i+1,pip+6);
+	return dp[i][pip]=calc(i+1,pip+1)+calc(i+1,pip+2)+calc(i+1,pip+3)+calc(i+1,pip+4)+calc(i+1,pip+5)+calc(i+1,pip+6);
 }
 int main(int argc, char const *argv[])
 {
 	int t;
 	scanf("%d",&t);
 	while(t--){
+		init();
 		scanf("%lld %lld",&n,&k);
 		printf("%d\n",calc(0,0));
 	}
