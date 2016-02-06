736fe80c81022ef5d549e867378fd8fa0c00b246
AadityaJ <aadityajamuar@gmail.com>
2016-02-06 23:20:10 +0530

    adding

--- Feb2016/cpcr1c.c
+++ Feb2016/cpcr1c.c
@@ -1,8 +1,9 @@
 //http://www.spoj.com/problems/CPCRC1C/
 #include <stdio.h>
-int sum_digits[100];
-int calc_dgit_sum(long long int n){
-	int sum=0;
+#define num 1000000000
+int sum_digits[num];
+long long int calc_dgit_sum(long long int n){
+	long long int sum=0;
 	while(n){
 		sum+=n%10;
 		n/=10;
@@ -13,9 +14,10 @@ int main(int argc, char const *argv[])
 {
 	long long int a,b;
 	while(1){
-		scanf("%d %d",&a,&b);
-		for(int i=1;i<=b;i++) sum_digits[i]=sum_digits[i-1]+calc_dgit_sum(i);
-		printf("%d\n",(sum_digits[b]-sum_digits[a]));
+		scanf("%lld %lld",&a,&b);
+		if(a==-1 && b==-1) break;
+		for(long long int i=1;i<=b;i++) sum_digits[i]=sum_digits[i-1]+calc_dgit_sum(i);
+		printf("%d\n",(sum_digits[b]-sum_digits[a]+1));
 	}
 	return 0;
 }
