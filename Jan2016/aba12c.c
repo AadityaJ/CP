ee9edddb492cea28662ba281c4dbe426df6f5006
AadityaJ <aadityajamuar@gmail.com>
2016-02-07 12:12:49 +0530

    adding

--- Jan2016/aba12c.c
+++ Jan2016/aba12c.c
@@ -4,29 +4,28 @@
 int k;
 int arr[105];
 int min(int a,int b){
+	if(!a || a==-1) return b;
+	if(!b || b==-1) return a;
 	return (a<b)?a:b;
 }
-int calc(int i,int cost,int weight)
-{
-	if(weight > k) 	return max;
-	if(weight ==  k) return cost;
-	int mi=0;
-	for(int j=1;j<=k;j++){
-		if(arr[j]==-1) continue;
-		int x = min(calc(i+1,cost+arr[i],i+j),calc(i+1,cost,i));
-		if(mi > x) mi = x; 
-	}
-	return mi;
+int calc(int i,int sum)
+{	//printf("%d %d\n",i,sum);
+	if(sum==k) return sum;
+	if(arr[i]==-1) return calc(i+1,sum);
+	/**case of -1 still left**/
+	if(i>k) return -1;
+	
+	return min(calc(i+1,sum+arr[i]),calc(i+1,sum));
 }
 int main(int argc, char const *argv[])
 {
 	int t;
 	scanf("%d",&t);
 	while(t--){
-		int n,k;
+		int n;
 		scanf("%d %d",&n,&k);
 		for(int i=1;i<=k;i++) scanf("%d",&arr[i]);
-		int res = calc(0,0,0);
+		int res = calc(1,0);
 		printf("%d\n",res);		
 	}
 	return 0;
