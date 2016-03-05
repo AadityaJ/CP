3504ef9c25002d84c87d96ec464f014050745735
AadityaJ <aadityajamuar@gmail.com>
2016-03-05 22:34:58 +0530

    adding files

--- Mar2016/ANARC09A.c
+++ Mar2016/ANARC09A.c
@@ -3,9 +3,22 @@
 int main(int argc, char const *argv[])
 {
 	char str[2001];
+	int index=1;
 	while(1){
 		scanf("%s",&str);
 		if(str[0]=='-') break;
+		int i=0;
+		int op=0,pa=0;
+		while(str[i]!='\0'){
+			if(str[i]=='{') op++;
+			else{
+				op--;
+				if(op<0){op=1;pa++;}
+			}
+			i++;
+		}
+		int val=pa+op/2;
+		printf("%d. %d\n",index ++,val);
 	}
 	return 0;
 }
---
+++ Mar2016/stpar.cpp
@@ -0,0 +1,10 @@
+//http://www.spoj.com/problems/STPAR/
+#include <iostream>
+#include <stack>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	stack <int> s;
+	
+	return 0;
+}
