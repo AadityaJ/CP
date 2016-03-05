cb309b7cfed74a94d6dc008ce5d59e3ec63e9fcc
AadityaJ <aadityajamuar@gmail.com>
2016-03-05 20:00:46 +0530

    adding

---
+++ Mar2016/ANARC09A.c
@@ -0,0 +1,11 @@
+//http://www.spoj.com/problems/ANARC09A/
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	char str[2001];
+	while(1){
+		scanf("%s",&str);
+		if(str[0]=='-') break;
+	}
+	return 0;
+}
