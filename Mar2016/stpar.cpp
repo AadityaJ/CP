dfc424e5cde0eb4b0aca8f959d193a67238891b4
AadityaJ <aadityajamuar@gmail.com>
2016-03-06 00:56:32 +0530

    done 2 questions

--- Mar2016/stpar.cpp
+++ Mar2016/stpar.cpp
@@ -1,48 +1,38 @@
 //http://www.spoj.com/problems/STPAR/
 #include <iostream>
+#include <stack>
 using namespace std;
-// for any number l :: l+1 should either be just in front or behind
-// shitty logic think again
-int arr[1001];
-int min(int num){
-	int min=1010;
-	for(int i=0;i<num;i++){
-		if(min > arr[i]) min = arr[i];
-	}
-	return min;
-}
-int max(int num){
-	int max=0;
-	for(int i=0;i<num;i++){
-		if(max < arr[i]) max = arr[i];
-	}
-	return max;
-}
-bool find(int n,int x){
-	bool res=0;
-	for(int i=0;i<n;i++){
-		if(arr[i]==(x+1)) res=1;
-	}
-	return res;
-}
+//logic : keep a lookout for number . push else into stack
 int main(int argc, char const *argv[])
 {
-	int num;
+	
 	while(1){
+		stack<int> mys;
+		int num;
+		int arr[1001];
+		int sor[1001];
 		cin>>num;
 		if(!num) break;
 		for(int i=0;i<num;i++) {cin>>arr[i];}
 		bool res = 0;
 		//res = 1 => no else yes
-		int mi = min(num);
-		int mx = max(num);
+		//lets get going
+		int no=1;
+		int p=0;
 		for(int i=0;i<num;i++){
-			if(arr[i]=mi || arr[i]==mx) continue;
-			if(arr[i]+1==arr[i+1] || find(i,arr[i])) continue;
-			res = 1;
+			if(arr[i]==no){sor[p++]=arr[i];no++;}
+			else {mys.push(arr[i]);}
 		}
-		if(res) cout<<"yes"<<endl;
-		else cout<<"no"<<endl;
+		while(!mys.empty()){
+			sor[p++]=mys.top();
+			mys.pop();
+		}
+		// is sor sorted
+		for(int i=0;i<num;i++){
+			if(sor[i]!=(i+1)) res=1;
+		}
+		if(res) cout<<"no \n";
+		else cout<<"yes \n";
 	}
 
 	return 0;
