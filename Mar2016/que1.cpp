755cf21d2a6383552b50ca6d8c3a5be1f0e4d11d
AadityaJ <aadityajamuar@gmail.com>
2016-03-16 11:14:38 +0530

    done

--- Mar2016/que1.cpp
+++ Mar2016/que1.cpp
@@ -1,15 +1,15 @@
 //http://www.spoj.com/problems/QUE1/
-//this is an erronus algorithm
 #include <queue>
 #include <iostream>
 using namespace std;
-int arr[1001],height[1001];
-bool more_than(int num,int par[],int n,int a){
-	int t=0;
+int arr[1001],height[1001],weights[1001];
+int get_num(int index,int n){
+	int num=arr[index];
+	int val=1;
 	for(int i=0;i<n;i++){
-		if(par[i] > num) t++;
+		if(arr[i]<num) val++;
 	}
-	return t==a;
+	return val;
 }
 int main(int argc, char const *argv[])
 {
@@ -21,6 +21,14 @@ int main(int argc, char const *argv[])
 		cin>>n;
 		for(int i=0;i<n;i++) cin>>arr[i];
 		for(int i=0;i<n;i++) cin>>height[i];
+		for(int i=0;i<n;i++){
+			weights[i]=get_num(i,n);
+		}
+		int fin_arr[1001];
+		for(int i=0;i<n;i++){
+			//if weights are same store smallest ones get printed and all else is stored in the queue
+			
+		}
 
 	}
 	return 0;
