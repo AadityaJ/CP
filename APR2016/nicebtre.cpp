31ae52b09b1fcb133d3e31cb239649b4785e1083
AadityaJ <aadityajamuar@gmail.com>
2016-04-03 12:57:01 +0530

    done

--- APR2016/nicebtre.cpp
+++ APR2016/nicebtre.cpp
@@ -1,8 +1,23 @@
 //http://www.spoj.com/problems/NICEBTRE/
 #include <iostream>
+// required help :( idk why
 using namespace std;
+int i;
+char s[1001];
+int rec(){
+	if (s[i++] =='l')
+             return 1;
+        else
+            return max(rec(),rec())+1;
+}
 int main(int argc, char const *argv[])
 {
-	
+	int t;
+	cin>>t;
+	while(t--){
+		i=0;
+		cin>>s;
+		cout<<(rec()-1)<<endl;
+	}
 	return 0;
 }
