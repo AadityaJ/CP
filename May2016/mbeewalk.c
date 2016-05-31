b44af344a9a275d3bf174455832a7e621676030b
AadityaJ <aadityajamuar@gmail.com>
2016-05-31 20:39:34 +0530

    adding

---
+++ May2016/mbeewalk.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int calc(int n){
+	int arr
+
+}
+int main(int argc, char const *argv[])
+{
+	int t;
+	scanf("%d",&t);
+	while(t--){
+		int n;
+		scanf("%d",&n);
+		int c = calc(n);
+		printf("%d\n",c);
+	}
+	return 0;
+}
