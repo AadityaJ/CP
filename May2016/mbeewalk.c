b5ed65797392fbd6fcd667d0ef8169af1965dd86
AadityaJ <aadityajamuar@gmail.com>
2016-05-31 20:49:55 +0530

    adding

--- May2016/mbeewalk.c
+++ May2016/mbeewalk.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
+#define n 17
+int way[15][n][n];
+int list[15];
+void calc(){
 
-int calc(int n){
-	int arr
-
+	way[0][8][8] = 1;
+    for ( int k = 1; k <= 14; k++) {
+        for ( int i = 1; i < n; i++ ) {
+            for ( int j = 1; j < n; j++ ) {
+                way[k][i][j] = way[k-1][i][j+1]+way[k-1][i][j-1]
+                                +way[k-1][i+1][j]+way[k-1][i-1][j]
+                                +way[k-1][i+1][j-1]+way[k-1][i-1][j+1];
+            }
+        }
+        list[k] = way[k][8][8];
+    }
 }
 int main(int argc, char const *argv[])
 {
 	int t;
 	scanf("%d",&t);
+	calc();
 	while(t--){
-		int n;
-		scanf("%d",&n);
-		int c = calc(n);
-		printf("%d\n",c);
+		int num;
+		scanf("%d",&num);
+		printf("%d\n",list[num]);
 	}
 	return 0;
 }
