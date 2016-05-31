d5d4259fa1c1757641945a32ea88d723072ea3cc
AadityaJ <aadityajamuar@gmail.com>
2016-05-31 15:31:29 +0530

    adding

--- May2016/msched.c
+++ May2016/msched.c
@@ -1,11 +1,21 @@
 //http://www.spoj.com/problems/MSCHED/
-#include <bits/stdc++.h>
+#include <iostream>
+#include <utility>
 
 using namespace std;
 int main(int argc, char const *argv[])
 {
 	int n;
 	cin>>n;
-	int mat
+	pair <int,int> act[1000];
+	for(int i=0;i<n;i++)
+		cin>>act[i].second>>act[i].first;
+	sort(act,act+n);
+	//for(int i=0;i<n;i++)
+	//	cout<<act[i].second<<"\t"<<act[i].first<<endl;
+	int i=0,t=act[0].second,max;
+	while(act[i++].second==t)
+	int count_milk = act[0].
+	for(int )
 	return 0;
 }
