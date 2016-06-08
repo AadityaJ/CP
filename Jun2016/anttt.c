914de1e1450cfa83ff0f97197c2f3dae53e9617a
AadityaJ <aadityajamuar@gmail.com>
2016-06-09 00:29:24 +0530

    adding

--- Jun2016/anttt.c
+++ Jun2016/anttt.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+
+int get_line_intersection(int p0_x, int p0_y, int p1_x, int p1_y, 
+    int p2_x, int p2_y, int p3_x, int p3_y, int *i_x, int *i_y)
+{
+    int s1_x, s1_y, s2_x, s2_y;
+    s1_x = p1_x - p0_x;     s1_y = p1_y - p0_y;
+    s2_x = p3_x - p2_x;     s2_y = p3_y - p2_y;
+
+    int s, t;
+    s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
+    t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
+
+    if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
+    {
+        // Collision detected
+        if (i_x != NULL)
+            *i_x = p0_x + (t * s1_x);
+        if (i_y != NULL)
+            *i_y = p0_y + (t * s1_y);
+        return 1;
+    }
+
+    return 0;
+}
 int main(int argc, char const *argv[])
 {
 	int t;
---
+++ Jun2016/foxlings.c
@@ -0,0 +1,56 @@
+//http://www.spoj.com/problems/FOXLINGS/
+#include <stdio.h>
+#include <algorithm>
+int arr[100];
+int size[100];
+void make_set(int n){
+	for(int i=0;i<n;i++){
+		arr[i]=i;
+		size[i]=0;
+	}
+}
+int find_set(int x){
+	// find parent of x
+	// arr[x] is the father of x
+	if(x!=arr[x])
+		arr[x]=find_set(arr[x]);
+	return arr[x];
+}
+void link(int a,int b){
+	if(size[a]>size[b])
+		arr[b]=a;
+	else{
+		arr[a]=b;
+		if(size[a]==size[b])
+			size[b]+=1;
+	}
+}
+void uni(int a,int b){
+	link(find_set(a),find_set(b));
+}
+int main(int argc, char const *argv[])
+{
+	//int n;
+	//scanf("%d",&n);
+	int n,m;
+	int x,y;
+	scanf("%d %d",&n,&m);
+	make_set(n+1);
+	for(int i=0;i<m;i++){
+		scanf("%d %d",&x,&y);
+		uni(x,y);
+	}
+	//printf("\n");
+	//for(int i=1;i<=n;i++)
+	//	printf("%d ",arr[i]);
+	//printf("\n");
+	std::sort(arr,arr+(n+1));
+	//for(int i=1;i<=n;i++)
+	//	printf(" %d ",arr[i]);
+	int count=1;
+	for(int i=2;i<=n;i++){
+		if(arr[i]!=arr[i-1]) count++;
+	}
+	printf("%d\n",count);
+	return 0;
+}
