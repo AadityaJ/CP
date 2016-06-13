14edef600901767f997dc60bfb2cb0b6c6c5c0fe
AadityaJ <aadityajamuar@gmail.com>
2016-06-13 23:40:37 +0530

    done

--- Jun2016/permu_len.cpp
+++ Jun2016/permu_len.cpp
@@ -2,17 +2,20 @@
 #include <string>
 #include <iostream>
 using namespace std;
-string swap(int a,int b,string text){
-	char temp=text[a];
-	text[a]=text[b];
-	text[b]=temp;
-	return text;
+// all 0 at starting and then change
+bool notIn(bool arr[],int index){
+	return !(arr[index]);
 }
-void printAll(string text,int str_len){
-	for(int i=0;i<str_len;i++){
-		for(int j=i+1;j<str_len;j++){
-			string st=swap(i,j,text);
-			cout<<st<<endl;
+
+
+void printAll(string text,int index,int str_len,bool occur[],string str){
+	if(index==0){cout<<str<<endl;return;}
+	for(int i=0;i<index;i++){
+		if(notIn(occur,index)){
+			occur[index]=1;
+			str+=text[index];
+			printAll(text,index-1,str_len,occur,str);
+			occur[index]=0;
 		}
 	}
 
@@ -22,6 +25,8 @@ int main(int argc, char const *argv[])
 	string str="";
 	string text="ABCD";
 	int str_len=4;
-	printAll(text,str_len);
+	bool occur[4];
+	for(int i=0;i<str_len;i++) occur[i]=0;
+	printAll(text,str_len,str_len,occur,str);
 	return 0;
 }
