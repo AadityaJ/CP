b2a5b81bcb9037a9d0236a449ce1b98b85494970
AadityaJ <aadityajamuar@gmail.com>
2016-06-15 23:27:53 +0530

    adding

---
+++ Jun2016/xmax.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	scanf("%d",&n);
+	int num[n];
+	for(int i=0;i<n;i++){scanf("%d",&num[i])};
+	// now maximize
+	return 0;
+}
