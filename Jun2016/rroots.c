8c154a045ce93709901c32e5ba605742c44d037b
AadityaJ <aadityajamuar@gmail.com>
2016-06-19 23:20:12 +0530

    adding adhoc

---
+++ Jun2016/rroots.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int t,s;
+	double sq2 = sqrt(2);
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%d",&s);
+		double x = 3*sqrt(s);
+		double ans = (x - sq2)/x;
+		printf("%.6lf\n",ans);
+	}
+	return 0;
+}
