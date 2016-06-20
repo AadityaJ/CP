49a00d050e47843b87981aaafea95edbd3224336
AadityaJ <aadityajamuar@gmail.com>
2016-06-20 23:07:37 +0530

    adding adhoc

---
+++ Jun2016/ae1b.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+#define MAX 1111
+int main(int argc, char const *argv[])
+{
+	int a[MAX], n, s, t, i, r, d, c;
+	scanf("%d%d%d", &n, &s, &t);
+	for(i=0; i<n; i++) scanf("%d", &a[i]);
+	sort(a, a+n);
+	r = s*t;
+	d = c = 0;
+	for(i=n-1; i>=0; i--)
+	{
+		d += a[i];
+		c++;
+		if(d >= r) break;
+	}
+	printf("%d\n", c);
+	return 0;
+	return 0;
+}
