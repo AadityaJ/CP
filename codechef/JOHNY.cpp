e1c87dd97a5b16ec6f5cbc961aa50eb74d79e98f
AadityaJ <aadityajamuar@gmail.com>
2016-08-27 23:16:34 +0530

    adding

---
+++ codechef/JOHNY.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(int argc, char const *argv[]) {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++) cin>>arr[i];
+        cin>>x;
+        int ele=arr[x-1];
+        // find out nos smaller than ele
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(ele>arr[i]) cnt++;
+        }
+        cout<<cnt+1<<"\n";
+    }
+    return 0;
+}
