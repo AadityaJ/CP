fc32780d9bf3cb271a10a40c96d69f16fd435e3a
AadityaJ <aadityajamuar@gmail.com>
2016-09-02 11:47:33 +0530

    adding

---
+++ codechef/fLIPCOIN.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/FLIPCOIN
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[]) {
+    int n,q;
+    cin>>n>>q;
+    bool arr[n]={0};
+    while(q--){
+        int sw,a,b;
+        cin>>sw>>a>>b;
+        
+    }
+    return 0;
+}
