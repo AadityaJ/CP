149f9fa63cefc781df72ba3e3799163d087e38c7
AadityaJ <aadityajamuar@gmail.com>
2016-09-04 00:25:32 +0530

    issues

--- codechef/dIVMAC.cpp
+++ codechef/dIVMAC.cpp
@@ -18,7 +18,19 @@ int main(int argc, char const *argv[]) {
     int t;
     cin>>t;
     while(t--){
-        
+        int n,m;
+        cin>>n>>m;
+        int arr[n],prime_arr[n];
+        // create Prime arr
+        for(int i=0;i<n;i++) cin>>arr[i];
+        for(int i=0;i<n;i++){
+            int j=2;
+            for(j=2;prime[j]&& j<=arr[i] && (arr[i]%j!=0);j++) ; // keep doing till u get here
+            if(prime[j]>arr[i]) prime_arr[i]=1;
+            else prime_arr[i]=j;
+        }
+        for(int i=0;i<n;i++) cout<<prime_arr[i]<<"  ";
+        cout<<endl;
     }
     return 0;
 }
