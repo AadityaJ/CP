af0227ecc3e1dc495c2ffe606bf2d9e122c02114
AadityaJ <aadityajamuar@gmail.com>
2016-09-10 20:14:00 +0530

    cyclic method left

--- codechef/cHEFRRUN.cpp
+++ codechef/cHEFRRUN.cpp
@@ -4,12 +4,23 @@
 using namespace std;
 
 class Graph{
-    std::vector<int> v[MAX];
 public:
+    std::vector<int> v[MAX];
+    int node;
     bool isCyclic(int node);
     int countCycles();
     void addEdge(int a,int b){v[x].push_back(y);}
 };
+
+int Graph::countCycles(){
+    bool visited[n],resstack[n];
+    for(int i=1;i<=node;i++){visited[i]=0;resstack[i]=0;}
+    int count=0;
+    for(int i=1;i<=n;i++){
+        count+=isCyclic(i);
+    }
+    return count;
+}
 int main(int argc, char const *argv[]) {
     int t;
     cin>>t;
@@ -17,6 +28,7 @@ int main(int argc, char const *argv[]) {
         Graph g;
         int n;
         cin>>n;
+        g.node=n;
         int val;
         for(int i=1;i<=n;i++) {cin>>val; g.addEdge(i,((i+val+1)%n));}
         int count=0;
