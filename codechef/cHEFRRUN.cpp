853d09203e67dc9b06c161bf859d667f8057df07
AadityaJ <aadityajamuar@gmail.com>
2016-09-11 12:23:26 +0530

    doing but errors

--- codechef/cHEFRRUN.cpp
+++ codechef/cHEFRRUN.cpp
@@ -6,18 +6,35 @@ using namespace std;
 class Graph{
 public:
     std::vector<int> v[MAX];
-    int node;
-    bool isCyclic(int node);
+    int n;
+    bool isCyclic(int node,bool *,bool *);
     int countCycles();
-    void addEdge(int a,int b){v[x].push_back(y);}
+    void addEdge(int x,int y){v[x].push_back(y);}
+    void printGraph();
 };
-
+void Graph::printGraph(){
+    
+}
+bool Graph::isCyclic(int node,bool *visited,bool *resstack){
+    if(visited[node]==false){
+        resstack[node]=1;
+        visited[node]=1;
+        for(int i=0;i<v[node].size();i++){
+            if(!visited[v[node][i]] && isCyclic(v[node][i],visited,resstack))
+                return 1;
+            else if(resstack[v[node][i]]) return 1;
+        }
+    }
+    resstack[node]=0;
+    return 0;
+}
 int Graph::countCycles(){
-    bool visited[n],resstack[n];
-    for(int i=1;i<=node;i++){visited[i]=0;resstack[i]=0;}
+    bool visited[n+1],resstack[n+1];
+    for(int i=1;i<=n;i++){visited[i]=0;resstack[i]=0;}
     int count=0;
     for(int i=1;i<=n;i++){
-        count+=isCyclic(i);
+        count+=isCyclic(i,visited,resstack);
+        cout<<i<<" "<<count<<" ";
     }
     return count;
 }
@@ -26,11 +43,11 @@ int main(int argc, char const *argv[]) {
     cin>>t;
     while(t--){
         Graph g;
-        int n;
-        cin>>n;
-        g.node=n;
+        int no;
+        cin>>no;
+        g.n=no;
         int val;
-        for(int i=1;i<=n;i++) {cin>>val; g.addEdge(i,((i+val+1)%n));}
+        for(int i=1;i<=no;i++) {cin>>val; g.addEdge(i,((i+val+1)%no));}
         int count=0;
         /*for(int i=0;i<n;i++){
             int j=i;
@@ -43,7 +60,7 @@ int main(int argc, char const *argv[]) {
 
         }*/
         count=g.countCycles();
-        cout<<count<<endl;
+        cout<<endl<<count<<endl;
     }
     return 0;
 }
