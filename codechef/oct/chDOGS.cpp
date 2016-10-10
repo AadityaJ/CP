bccc07e6b0e7df6d820d0b83b40063c1bd584dab
AadityaJ <aadityajamuar@gmail.com>
2016-10-10 18:38:07 +0530

    finally done :)

--- codechef/oct/chDOGS.cpp
+++ codechef/oct/chDOGS.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <iomanip>
+#define vlow 1e-6
 //2s/3v
+//error was to set precision to certain value :)
 using namespace std;
 int main(int argc, char const *argv[]) {
     int t;
@@ -9,7 +12,9 @@ int main(int argc, char const *argv[]) {
         double res;
         cin>>s>>v;
         res=((2.00*s)/(3.00*v));
-        cout<<res<<endl;
+        cout<<fixed<<setprecision(7);
+        if(res<=vlow){res=0.0000001;cout<<res<<endl;}
+        else {cout<<res<<endl;}
     }
     return 0;
 }
