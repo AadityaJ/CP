1b59d5f120c425e0042c72193c71ef0c115a0de1
AadityaJ <aadityajamuar@gmail.com>
2016-10-10 20:24:04 +0530

    20 pts

---
+++ codechef/oct/seAARI.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[]) {
+    //lets see if this works
+    int n,k,d;
+    cin>>n>>k>>d;
+    int arr[n];
+    int avg_diff=0;
+    int last=0;
+    int temp;
+    for(int i=0;i<n;i++){
+        cin>>temp;
+        arr[i]=temp;
+        avg_diff+=(temp-last);
+        last=temp;
+    }
+    avg_diff/=n;
+    //cout<<avg_diff<<"\n";
+    cout<<arr[0]<<" "<<avg_diff;
+    cout<<"\n-1";
+    return 0;
+}
