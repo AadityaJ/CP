c83c2353951730a743bdfbd45bd3a7b7624f62fc
AadityaJ <aadityajamuar@gmail.com>
2016-10-16 13:36:52 +0530

    issue pertain

--- codechef/oct/bgQRS.cpp
+++ codechef/oct/bgQRS.cpp
@@ -10,14 +10,6 @@ void ap(unsigned long long int *arr,long long int l,long long int r,long long in
         diff+=x;
     }
 }
-/*long long int calcTrai(long long long long int x){
-    long long int res=0;
-    while(x){
-        if(x%10==0) res++;
-        x/=10;
-    }
-    return res;
-}*/
 long long int numPowof(long long int x,long long int mod){
     long long int count=0;
     if(x%mod!=0) return count;
@@ -31,22 +23,12 @@ long long int numPowof(long long int x,long long int mod){
 long long int zero(unsigned long long int *arr,long long int l,long long int r){
     long long int num_twos=0,num_fives=0,num_tens=0;
     for(long long int i=l;i<=r;i++){
-        //prod*=arr[i];
-        //if(arr[i]%10==0) num_tens++;
-        //else if(arr[i]%5==0) num_fives++;
-        //else if(arr[i]%2==0) num_twos++;
-        //num+=min(numPowof(arr[i],2),numPowof(arr[i],5))+numPowof(arr[i],10);
         num_tens+=numPowof(arr[i],10);
         num_twos+=(numPowof(arr[i],2)-numPowof(arr[i],10));
         num_fives+=(numPowof(arr[i],5)-numPowof(arr[i],10));
     }
-    //cout<<num_tens<<" "<<num_twos<<" "<<num_fives<<endl;
     return num_tens+min(num_twos,num_fives);
 }
-/*void prlong long intArr(long long int *arr,long long int n){
-    for(long long int i=1;i<=n;i++) cout<<arr[i]<<" ";
-    cout<<endl;
-}*/
 int main(int argc, char const *argv[]) {
     long long int t;
     cin>>t;
@@ -58,7 +40,6 @@ int main(int argc, char const *argv[]) {
         for(long long int i=1;i<=n;i++){cin>>arr[i];}
         long long int l,r,x,type;
         long long int ans=0;
-        //cout<<zero(arr,2,5);
         for(long long int i=0;i<m;i++){
             cin>>type;
             //cout<<type<<endl;
