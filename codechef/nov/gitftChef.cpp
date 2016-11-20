13f8330f2f4a1075aa0180dc3906a8c3a5ad6ccc
AadityaJ <aadityajamuar@gmail.com>
2016-11-20 13:03:41 +0530

    3rd done

---
+++ codechef/nov/gitftChef.cpp
@@ -0,0 +1,12 @@
+//https://www.codechef.com/problems/GIFTCHEF
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char const *argv[]) {
+    int t;
+    cin>>t;
+    while(t--){
+        
+    }
+    return 0;
+}
