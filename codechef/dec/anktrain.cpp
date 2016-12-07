e56d73cab612c95fe482df43ed6777a39ca45d6c
AadityaJ <aadityajamuar@gmail.com>
2016-12-07 19:13:07 +0530

    adding

--- codechef/dec/anktrain.cpp
+++ codechef/dec/anktrain.cpp
@@ -2,20 +2,34 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int map[8]={4,5,6,1,2,3,8,7}
+int map[8]={4,5,6,1,2,3,8,7};
 string getType(int a){
     a=(a%8);
     a++;
-    cout<<a<<endl;
+    //cout<<a<<endl;
     switch(a){
-        case 
+        case 1: return "LB";
+        case 4: return "LB";
+        case 7: return "SL";
+        case 8: return "SU";
+        case 2: return "MB";
+        case 5: return "MB";
+        case 3: return "UB";
+        case 6: return "UB";
     }
 }
 int main(int argc, char const *argv[]) {
     int t;
     cin>>t;
     while(t--){
-
+        int n;
+        cin>>n;
+        int x=(n%8)+1;
+        int y=(n/8);
+        int x1=map[x-1];
+        int re=x1*y;
+        string result=getType(x)+to_string(re);
+        cout<<result<<endl;
     }
     return 0;
 }
