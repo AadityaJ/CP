f0507e94518edbc609ca47f2b03fdbe4cffe0832
AadityaJ <aadityajamuar@gmail.com>
2016-12-07 22:21:23 +0530

    adding 2

--- codechef/dec/kirlab.cpp
+++ codechef/dec/kirlab.cpp
@@ -13,18 +13,12 @@ int gcd(int n1,int n2){
 int lis( int arr[], int n )
 {
     int lis[n], i, j, max = 0;
-
-    /* Initialize LIS values for all indexes */
     for (i = 0; i < n; i++ )
         lis[i] = 1;
-
-    /* Compute optimized LIS values in bottom up manner */
     for (i = 1; i < n; i++ )
         for (j = 0; j < i; j++ )
             if ( gcd(arr[i],arr[j])>1 && lis[i] < lis[j] + 1)
                 lis[i] = lis[j] + 1;
-
-    /* Pick maximum of all LIS values */
     for (i = 0; i < n; i++ )
         if (max < lis[i])
             max = lis[i];
