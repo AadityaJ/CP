9090d462925002febe2318ef3ca63991f75d9172
AadityaJ <aadityajamuar@gmail.com>
2016-12-07 18:35:42 +0530

    adding

---
+++ codechef/dec/anktrain.cpp
@@ -0,0 +1,21 @@
+//https://www.codechef.com/DEC16/problems/ANKTRAIN
+#include <iostream>
+#include <string>
+using namespace std;
+int map[8]={4,5,6,1,2,3,8,7}
+string getType(int a){
+    a=(a%8);
+    a++;
+    cout<<a<<endl;
+    switch(a){
+        case 
+    }
+}
+int main(int argc, char const *argv[]) {
+    int t;
+    cin>>t;
+    while(t--){
+
+    }
+    return 0;
+}
