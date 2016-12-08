60ed11722801a1c527eeb71e85b927b0f66e3d66
AadityaJ <aadityajamuar@gmail.com>
2016-12-08 23:06:39 +0530

    adding

--- codechef/dec/base.cpp
+++ codechef/dec/base.cpp
@@ -2,21 +2,28 @@
 #include <cmath>
 #include <iostream>
 using namespace std;
+long int floor_log(long a,int b){
+    long int pow_er=b;
+    long int i=1;
+    for(i=1;i<=a;i++){
+        if(a<=pow_er)
+            return --i;
+        pow_er*=b;
+    }
+    return --i;
+}
 int main(int argc, char const *argv[]) {
     int t;
     cin>>t;
     while(t--){
+        cout<<floor_log(17179869184,2)<<endl;
         double n;
         cin>>n;
         if(n==0){cout<<"0"<<endl;continue;}
         if(n==1){cout<<"INFINITY\n";continue;}
         double count=1;
-        //cout<<"\n\nFOR NUMBER  :: "<<n<<endl;
-        cout<<log2(1024)<<endl;
         for(int i=3;i<=n;i++){
-            //double j=pow(i,(int)((int)log(n)/(int)log(i)));
             int j=pow(i,(int)((log2(n))/log2(i)));
-            //cout<<"BASE ::"<<i<<" "<<j<<endl;
             if(j<=n && n<(2*j)) count++;
         }
         std::cout << count << std::endl;
