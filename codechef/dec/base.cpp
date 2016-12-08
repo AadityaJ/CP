81870c703bac447d4461e2bae97d1208b26b4ff1
AadityaJ <aadityajamuar@gmail.com>
2016-12-08 21:24:52 +0530

    issues

--- codechef/dec/base.cpp
+++ codechef/dec/base.cpp
@@ -1,19 +1,21 @@
 //https://www.codechef.com/DEC16/problems/BASE
-#include <math.h>
+#include <cmath>
 #include <iostream>
 using namespace std;
 int main(int argc, char const *argv[]) {
     int t;
     cin>>t;
     while(t--){
-        int n;
+        double n;
         cin>>n;
         if(n==0){cout<<"0"<<endl;continue;}
         if(n==1){cout<<"INFINITY\n";continue;}
-        int count=1;
+        double count=1;
         //cout<<"\n\nFOR NUMBER  :: "<<n<<endl;
-        for(int i=2;i<n;i++){
-            int j=pow(i,(int)(log(n)/log(i)));
+        cout<<log2(1024)<<endl;
+        for(int i=3;i<=n;i++){
+            //double j=pow(i,(int)((int)log(n)/(int)log(i)));
+            int j=pow(i,(int)((log2(n))/log2(i)));
             //cout<<"BASE ::"<<i<<" "<<j<<endl;
             if(j<=n && n<(2*j)) count++;
         }
