4dedbd1866b949ae2b30010f27128284a77ebbed
AadityaJ <aadityajamuar@gmail.com>
2016-12-16 23:41:55 +0530

    adding . EDITORIALS

--- codechef/dec/kirlab.cpp
+++ codechef/dec/kirlab.cpp
@@ -1,8 +1,18 @@
 //https://www.codechef.com/DEC16/problems/KIRLAB
 #include <iostream>
+#define MAX 10000001
 using namespace std;
 int min(int a,int b){
     return (a>b)?b:a;
+}
+int smallest[MAX];
+void seive(){
+    for(int i=2;i<MAX;i++){
+        if(smallest[i]==0){
+            smallest[i]=i;
+            for(int j=i;j<MAX;j+=i) smallest[j]=i;
+        }
+    }
 }
 /*int gcd(int n1,int n2){
     while(n1 != n2){
@@ -36,7 +46,7 @@ int gcd(int a, int b) {
     }
     return 0;
 }*/
-int lis( int arr[], int n )
+/*int lis( int arr[], int n )
 {
     int lis[n], i, j;
     int max = 0;
@@ -52,8 +62,22 @@ int lis( int arr[], int n )
             max = lis[i];
 
     return max;
+}*/
+int dp(int *arr,int n){
+    int mat[10]={0};
+    for(int i=0;i<n;i++){
+        if(arr[i]==1) continue;
+        int factor[10];
+        int x=smallest[arr[i]];
+        int lst=arr[i],temp=1;
+        factor[0]=x;
+        while(x!=i){
+            if(x!=arr[i]) factor[temp++];
+        }
+    }
 }
 int main(int argc, char const *argv[]) {
+    seive();
     int t;
     cin>>t;
     while(t--){
@@ -61,7 +85,7 @@ int main(int argc, char const *argv[]) {
         cin>>n;
         int arr[n];
         for(int i=0;i<n;i++) cin>>arr[i];
-        cout<<lis(arr,n)<<endl;
+        //cout<<lis(arr,n)<<endl;
     }
     return 0;
 }
---
+++ codechef/dec/kirlab_sol.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+int sieve[10000001],prm[11][700000],coun,sieve2[10000001];
+void comp()
+{
+    int t=2;
+    coun=2;
+    for(int i=1;i<=5000000;i++)
+    {
+        sieve[2*i]=2;
+        sieve2[2*i]=0;
+    }
+    for(int i=1;i<=3333333;i+=2)
+    {
+        sieve[3*i]=3;
+        sieve2[3*i]=1;
+    }
+    for(long long i=5;i<=10000000;i+=2)
+    {
+        if(sieve[i]==0)
+        {
+            sieve[i]=i;
+            sieve2[i]=coun;
+            for(long long j=i;(i<=10000)&&(i*j)<=10000000ll;j+=2)
+            {
+                if(sieve[i*j]==0)
+                {
+                    sieve[i*j]=i;
+                    sieve2[i*j]=coun;
+                }
+            }
+            coun++;
+        }
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    comp();
+    int t,n,x,x2,maxi,maxo,val;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        maxo=1;
+        for(int i=0;i<n;i++)
+        {
+            cin>>x;
+            x2=x;
+            maxi=0;
+            while(x2>=2)
+            {
+                val=sieve[x2];
+                maxi=max(maxi,prm[t][sieve2[x2]]);
+                while(sieve[x2]==val)   x2/=val;
+            }
+            maxi++;
+            maxo=max(maxi,maxo);
+            while(x>=2)
+            {
+
+                val=sieve[x];
+                prm[t][sieve2[x]]=maxi;
+                while(sieve[x]==val)    x/=val;
+            }
+        }
+        cout<<maxo<<endl;
+    }
+    return 0;
+}
