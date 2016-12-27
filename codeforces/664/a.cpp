99c32bbc438bf0e058235068c7521000c9367ee2
AadityaJ <aadityajamuar@gmail.com>
2016-12-27 11:33:32 +0530

    adding

--- codeforces/664/a.cpp
+++ codeforces/664/a.cpp
@@ -4,6 +4,9 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-	
+	string s1,s2;
+	cin>>s1>>s2;
+	if(s1.compare(s2)==0) cout<<s1;
+	else cout<<1;
 	return 0;
 }
