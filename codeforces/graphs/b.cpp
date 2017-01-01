235e537c3bd2657fc9f5f4a20ade072bac732b1e
AadityaJ <aadityajamuar@gmail.com>
2017-01-01 22:14:39 +0530

    adding

--- codeforces/graphs/b.cpp
+++ codeforces/graphs/b.cpp
@@ -1,9 +1,12 @@
+//http://codeforces.com/problemset/problem/520/B
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
+#define MAX 20001
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int n,m;
+	cin>>n>>m;
 	return 0;
 }
