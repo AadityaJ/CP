01825c1bbdb1a0d39ffd01aa603dfa084ab260e5
AadityaJ <aadityajamuar@gmail.com>
2017-01-01 13:10:25 +0530

    adding graph.gonna start

--- codeforces/graphs/a.cpp
+++ codeforces/graphs/a.cpp
@@ -1,9 +1,10 @@
+//http://codeforces.com/problemset/problem/115/A
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	
 	return 0;
 }
