f48ba68987c8be38d4f630f6e416ef5f11dcd874
AadityaJ <aadityajamuar@gmail.com>
2017-01-01 22:21:44 +0530

    adding

--- codeforces/graphs/b.cpp
+++ codeforces/graphs/b.cpp
@@ -3,10 +3,18 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#define MAX 20001
+#include <queue>
+#define MAX 10005
 using namespace std;
 int main(int argc, char const *argv[]) {
 	int n,m;
 	cin>>n>>m;
+	std::vector<int> d(MAX,-1);
+	queue <int> q;
+	d[n]=0;
+	q.enqueue(n);
+	while(!q.empty()){
+		
+	}
 	return 0;
 }
