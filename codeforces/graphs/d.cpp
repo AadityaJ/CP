326ce6755c1dadd8fa94c3bac2a2c09e89347cce
AadityaJ <aadityajamuar@gmail.com>
2017-01-02 19:57:51 +0530

    adding

--- codeforces/graphs/d.cpp
+++ codeforces/graphs/d.cpp
@@ -1,9 +1,10 @@
+//http://codeforces.com/problemset/problem/69/A
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	
 	return 0;
 }
