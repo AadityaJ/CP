ff0ea1aee22c90b926b4c7c5ff8eb99e48637871
AadityaJ <aadityajamuar@gmail.com>
2017-01-10 00:44:17 +0530

    jan begin

--- codechef/jan/1.cpp
+++ codechef/jan/1.cpp
@@ -3,7 +3,22 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+long long f(long long c,long long d){
+	if(c<(2*d)) return 4*d;
+	else return 4*d+(c-2*d)*4;
+}
+long long mx(long c,long d){
+	return 4*(c+d);
+}
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		long long int c,d,l;
+		cin>>c>>d>>l;
+		if(l%4!=0){cout<<"no\n";continue;}
+		if(l>=f(c,d) && l<=mx(c,d)) cout<<"yes\n";
+		else cout<<"no\n";
+	}
 	 return 0;
 }
