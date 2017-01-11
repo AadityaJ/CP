12c1b43cb162e9d9d476e0694634a5fec706a879
AadityaJ <aadityajamuar@gmail.com>
2017-01-11 19:18:59 +0530

    adding

--- codechef/jan/2.cpp
+++ codechef/jan/2.cpp
@@ -2,39 +2,56 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <utility>
+#include <queue>
 using namespace std;
+class CompareDist
+{
+public:
+    bool operator()(pair<int,int> n1,pair<int,int> n2) {
+        return n1.second<n2.second;
+    }
+};
 int main(int argc, char const *argv[]) {
 	int t;
 	cin>>t;
 	while(t--){
 		int n;
 		cin>>n;
-		int prod[n];
-		int arr[n][n];
-		int res[n][n];
-		for(int i=0;i<n;i++) cin>>prod[i];
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++) arr[i][j]=1;
-			arr[i][i]=0;
-		}
+		priority_queue<pair<int,int>,vector<pair<int,int> >,CompareDist> pq;
+		queue <pair<int,int> > q;
+		//int prod[n];
+		int x;
+		for(int i=0;i<n;i++) {cin>>x;pq.push(make_pair(i+1,x));}
+		//for(int i=0;i<n;i++) {cout<<pq.top().first<<" "<<pq.top().second<<" ";pq.pop();}
+		std::vector<int> G[5000];
 		for(int i=0;i<n-1;i++){
-			int x,y;
-			cin>>x>>y;
-			arr[x-1][y-1]=arr[y-1][x-1]=0;
+			int a,b;
+			cin>>a>>b;
+			G[b].push_back(a);
+			G[a].push_back(b);
 		}
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
-				if(arr[i][j]==0) res[i][j]=0;
-				else res[i][j]=prod[j];
+		/*for(int i=1;i<=n;i++){
+			cout<<endl<<i<<" : ";
+			for(int j=0;j<G[i].size();j++){
+				cout<<G[i][j]<<" ";
 			}
-		}
-		for(int i=0;i<n;i++){
-			int mx=0,index;
-			for(int j=0;j<n;j++){
-				if(mx<res[i][j]){mx=res[i][j];index=j;}
+		}*/
+		for(int i=1;i<=n;i++){
+			for(int j=0;j<G[i].size();j++){
+				pair<int,int> x;
+				if(pq.top().first==G[i][j]){
+					x.first=pq.top().first;
+					x.second=pq.top().second;
+					q.push(x);
+					pq.pop();
+				}
 			}
-				//cout<<res[i][j]<<" ";
-			cout<<index+1<<" ";
+			cout<<pq.top().first<<" ";
+			for(int j=0;j<q.size();j++){
+				pq.push(q.front());
+				q.pop();
+			}
 		}
 	}
 	 return 0;
