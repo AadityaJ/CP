9f684ab0209aa919cec88bb9cbfe9dc802794a99
AadityaJ <aadityajamuar@gmail.com>
2017-01-13 20:57:31 +0530

    adding

--- codechef/jan/3.cpp
+++ codechef/jan/3.cpp
@@ -8,8 +8,8 @@ int main(int argc, char const *argv[]) {
 	cin>>t;
 	while(t--){
 		int n,m;
-		string tnk[100];
 		cin>>n>>m;
+		string tnk[n];
 		for(int i=0;i<n;i++){
 			cin>>tnk[i];
 		}
@@ -17,7 +17,15 @@ int main(int argc, char const *argv[]) {
 			cout<<tnk[i];
 			cout<<endl;
 		}*/
-		
+		bool flg=0;
+		for(int i=0;i<n-1;i++){
+
+			for(int j=0;j<m;j++){
+				if(tnk[i][j]=='B'&&tnk[i+1][j]=='A'){flg=1;break;}
+			}
+		}
+		if(flg) cout<<"no\n";
+		else cout<<"yes\n";
 	}
 	 return 0;
 }
