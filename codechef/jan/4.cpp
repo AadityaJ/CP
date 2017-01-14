309160afd41390f616c1c6f644ff2ed646841983
AadityaJ <aadityajamuar@gmail.com>
2017-01-14 18:57:14 +0530

    adding

--- codechef/jan/4.cpp
+++ codechef/jan/4.cpp
@@ -7,6 +7,11 @@ int main(int argc, char const *argv[]) {
 	int t;
 	cin>>t;
 	while(t--){
+		int n,e;
+		cin>>n>>e;
+		std::vector<int> G[100];
+		if(n!=e){cout<<"No\n";continue;}
+		for(int i=0;i<e;i++){int x;cin>>x;G[i].push_back(x);}
 		
 	}
 	return 0;
