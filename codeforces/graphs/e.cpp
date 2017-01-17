f9b1a74b4461ef03ef86aa57dd2caf8042ad88ca
AadityaJ <aadityajamuar@gmail.com>
2017-01-17 19:09:12 +0530

    adding

--- codeforces/graphs/e.cpp
+++ codeforces/graphs/e.cpp
@@ -5,6 +5,23 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-	
+	int n,m;
+	cin>>n>>m;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	int total=0;
+	for(int i=0;i<m;i++){
+		int x,y;
+		cin>>x>>y;
+		total+=(arr[x-1]<arr[y-1])?arr[x-1]:arr[y-1];
+		cout<<i<<" "<<total<<endl;
+	}
+	cout<<total;
 	return 0;
 }
+/*
+1)Think simple.
+2)Its all about killing edges: hence greedy approach is worth inhabiting.
+*/
