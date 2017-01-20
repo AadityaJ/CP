4eea5975772642a6a89c07798a7e9bc6ebd97493
AadityaJ <aadityajamuar@gmail.com>
2017-01-20 09:12:19 +0530

    adding

--- codeforces/758/a.cpp
+++ codeforces/758/a.cpp
@@ -4,6 +4,18 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int n;
+	cin>>n;
+	int arr[n+1];
+	int mx=0;
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+		if(arr[i]>mx)mx=arr[i];
+	}
+	int sum=0;
+	for(int i=0;i<n;i++){
+		sum+=mx-arr[i];
+	}
+	cout<<sum;
 	return 0;
 }
--- codeforces/758/c.cpp
+++ codeforces/758/c.cpp
@@ -3,7 +3,23 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int arr[101][101];
 int main(int argc, char const *argv[]) {
-
+	int n,m;
+	long long int k;
+	int x,y;
+	cin>>n>>m>>k>>x>>y;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++) arr[i][j]=0;
+	}
+	int x_1=1,y_1=1;
+	for(int i=0;i<k;i++){
+		arr[x_1][y_1]++;
+		x_1++;
+	}
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++) cout<<arr[i][j]<<" ";
+		cout<<endl;
+	}
 	return 0;
 }
