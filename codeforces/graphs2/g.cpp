02740358925d73f8350ba00751147ba4788bc81e
AadityaJ <aadityajamuar@gmail.com>
2017-01-21 19:49:32 +0530

    adding

--- codeforces/graphs2/g.cpp
+++ codeforces/graphs2/g.cpp
@@ -4,6 +4,17 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int n,m;
+	cin>>n>>m;
+	string s1,s2;
+	cin>>s1>>s2;
+	string s;
+	s+=(s1[0]);
+	s+=(s1[s1.length()-1]);
+	s+=(s2[0]);
+	s+=(s2[s2.length()-1]);
+//	cout<<s<<endl;
+	if(s=="><^v"|| s=="<>v^"){cout<<"YES";}
+	else cout<<"NO";
 	return 0;
 }
