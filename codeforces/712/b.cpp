48e88e4996d6b2f92601c3432380dfa7cd0b1529
AadityaJ <aadityajamuar@gmail.com>
2017-01-27 10:24:11 +0530

    adding

--- codeforces/712/b.cpp
+++ codeforces/712/b.cpp
@@ -4,6 +4,18 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	string str;
+	cin>>str;
+	int n=str.length();
+	if(n%2==1) {cout<<"-1";return 0;}
+	int x=0,y=0;
+	for(int i=0;i<n;i++){
+		if(str[i]=='L') x--;
+		if(str[i]=='R') x++;
+		if(str[i]=='U') y++;
+		if(str[i]=='D') y--;
+	}
+	//cout<<x<<" "<<y<<endl;
+	cout<<(abs(x)+abs(y))/2;
 	return 0;
 }
--- codeforces/712/c.cpp
+++ codeforces/712/c.cpp
@@ -4,6 +4,12 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
+	int x,y;
+	cin>>x>>y;
+	int arr[3];
+	arr[0]=arr[1]=arr[2]=y;
+	while(!(arr[0]==x&&arr[1]==x&&arr[2]==x)){
 
+	}
 	return 0;
 }
