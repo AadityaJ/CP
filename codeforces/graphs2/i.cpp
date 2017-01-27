9837d5a1846b3ec4794339a8a125e63a8ad31760
AadityaJ <aadityajamuar@gmail.com>
2017-01-27 20:12:18 +0530

    adding

--- codeforces/graphs2/i.cpp
+++ codeforces/graphs2/i.cpp
@@ -5,6 +5,21 @@
 #define MAX 30010
 using namespace std;
 int main(int argc, char const *argv[]) {
-	
+	int n;
+	cin>>n;
+	int arr[n+1];
+	for(int i=1;i<=n;i++) cin>>arr[i];
+	int res[n+1];
+	for(int i=1;i<=n;i++) res[i]=1;
+	for(int i=1;i<=n;i++){
+		int temp=i;
+		while(arr[temp]!=-1){
+			res[i]++;
+			temp=arr[temp];
+		}
+	}
+	int mx=0;
+	for(int i=1;i<=n;i++) if(mx<res[i])mx=res[i];
+	cout<<mx;
 	return 0;
 }
