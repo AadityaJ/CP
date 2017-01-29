461a12bf8f577e61e8f8d9c304ad733733ccbe15
AadityaJ <aadityajamuar@gmail.com>
2017-01-29 21:00:42 +0530

    adding

--- codeforces/graphs2/i.cpp
+++ codeforces/graphs2/i.cpp
@@ -7,38 +7,13 @@ using namespace std;
 int G[MAX][MAX];
 int size[MAX];
 int main(int argc, char const *argv[]) {
-	int n,m;
-	cin>>n>>m;
-//	int G[n+1][n+1];
-//	int size[n+1];
-//	for(int i=1;i<=n;i++) size[i]=0;
-	for(int i=0;i<m;i++){
-		int a,b;
-		cin>>a>>b;
-		G[a][b]=1;
-		G[b][a]=1;
-		size[a]++;
-		size[b]++;
+	int n;
+	cin>>n;
+	string str[n+1];
+	for(int i=1;i<=n;i++) cin>>str[i];
+	std::vector<int> G[100];
+	for(int i=0;i<n-1;i++){
+		G[str[i][0]-'a']
 	}
-	//for(int i=1;i<=n;i++) cout<<size[i]<<" ";
-	//cout<<endl;
-	int mx=100000;
-	for(int i=1;i<=n;i++){
-		for(int j=i+1;j<=n;j++){
-			if(G[i][j]){
-				for(int k=j+1;k<=n;k++){
-					if(G[i][j]&&G[i][k]&&G[j][k]){
-						int val=size[i]-2;
-						val+=size[j]-2;
-						val+=size[k]-2;
-						if(val<mx) mx=val;
-	//					cout<<val<<endl;
-					}
-				}
-			}
-		}
-	}
-	if(mx==100000) cout<<"-1";
-	else cout<<mx;
 	return 0;
 }
