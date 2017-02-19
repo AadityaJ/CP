969e1f5b32aec3691ef01f52e9c531c07482f388
AadityaJ <aadityajamuar@gmail.com>
2017-02-19 12:16:48 +0530

    adding

--- codeforces/stuff/b.cpp
+++ codeforces/stuff/b.cpp
@@ -4,6 +4,13 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	string str;
+	cin>>str;
+	bool isOut=0;
+	for(int i=0;i<str.length();i++){
+		if(str[i]=='9'||str[i]=='H'||str[i]=='Q'){isOut=1;break;}
+	}
+	if(isOut) cout<<"YES";
+	else cout<<"NO";
 	return 0;
 }
--- codeforces/stuff/c.cpp
+++ codeforces/stuff/c.cpp
@@ -4,6 +4,12 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	string str;
+	cin>>str;
+	bool flg=1;
+	for(int i=1;i<str.length();i++){
+		if(!(str[i]<='Z')) {flg=0;break;}
+	}
+	
 	return 0;
 }
