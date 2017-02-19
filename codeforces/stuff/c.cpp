a278081bd7a1da7e90e47ace96ec7315501e2aac
AadityaJ <aadityajamuar@gmail.com>
2017-02-19 12:44:55 +0530

    adding

--- codeforces/stuff/c.cpp
+++ codeforces/stuff/c.cpp
@@ -10,6 +10,14 @@ int main(int argc, char const *argv[]) {
 	for(int i=1;i<str.length();i++){
 		if(!(str[i]<='Z')) {flg=0;break;}
 	}
-	
+	if(flg){
+		if(str[0]>'Z') str[0]=str[0]-'a'+'A';
+		else str[0]=str[0]-'A'+'a';
+		for(int i=1;i<str.length();i++){
+			str[i]=str[i]-'A'+'a';
+
+		}
+	}
+	cout<<str;
 	return 0;
 }
--- codeforces/stuff/d.cpp
+++ codeforces/stuff/d.cpp
@@ -3,7 +3,25 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+struct station{
+	int arr,dep;
+};
 int main(int argc, char const *argv[]) {
+	int a;
+	cin>>a;
 
+	station sn[a];
+	int cumu[a];
+	int sum_=0;
+	for(int i=0;i<a;i++){
+		cin>>sn[i].dep>>sn[i].arr;
+		sum_+=(sn[i].arr-sn[i].dep);
+		cumu[i]=sum_;
+	}
+	int mx=0;
+	for(int i=0;i<a;i++){
+		if(mx<cumu[i]) mx=cumu[i];
+	}
+	cout<<mx;
 	return 0;
 }
