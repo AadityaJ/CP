08f95b828240c658fe4cd5ef916e28210b1523d8
AadityaJ <aadityajamuar@gmail.com>
2017-02-21 23:50:55 +0530

    adding

--- codeforces/stuff2/b.cpp
+++ codeforces/stuff2/b.cpp
@@ -4,6 +4,19 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	string str,str_out;
+	int t;
+	cin>>t;
+	string ch;
+	for(int i=0;i<t;i++){
+		cin>>ch;
+		str+=ch;
+		str+="\n";
+	}
+	//cout<<str;
+	str_out="";
+	for(int i=0;i<t;i++){
+		
+	}
 	return 0;
 }
