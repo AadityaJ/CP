e51b9e40fa2055289fc1db9935d91b68b486b18b
AadityaJ <aadityajamuar@gmail.com>
2017-02-22 00:07:21 +0530

    adding

--- codeforces/stuff2/b.cpp
+++ codeforces/stuff2/b.cpp
@@ -4,19 +4,26 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-	string str,str_out;
+	std::string str,str_out="";
 	int t;
 	cin>>t;
-	string ch;
 	for(int i=0;i<t;i++){
+		string ch;
 		cin>>ch;
 		str+=ch;
 		str+="\n";
 	}
-	//cout<<str;
-	str_out="";
-	for(int i=0;i<t;i++){
-		
+	//cout<<str.length();
+	bool isit=0;
+	for(int i=0;i<str.length()-1;i++){
+		if(str[i]=='O'&&str[i+1]=='O'&&!isit){
+			isit=1;
+			str_out+="++";
+			i++;
+		}
+		else str_out+=str[i];
 	}
+	if(isit){cout<<"YES\n";cout<<str_out;}
+	else cout<<"NO";
 	return 0;
 }
