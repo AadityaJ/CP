7fff3cc5b0c88095cb5e1112c10cb402e599b82a
AadityaJ <aadityajamuar@gmail.com>
2017-02-24 10:55:51 +0530

    adding

--- codeforces/stuff2/c.cpp
+++ codeforces/stuff2/c.cpp
@@ -3,7 +3,15 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+bool g[3001][3001];
 int main(int argc, char const *argv[]) {
-
+	int n,m;
+	cin>>n>>m;
+	std::vector<int> neigh[n];
+	for(int i=0;i<m;i++){
+		int a,b;
+		cin>>a>>b;
+		
+	}
 	return 0;
 }
