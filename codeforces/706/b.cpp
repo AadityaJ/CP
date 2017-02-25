7f8134ea74f6e876b2704c0dfbb6770837c0cbcf
AadityaJ <aadityajamuar@gmail.com>
2017-02-25 18:33:38 +0530

    adding

--- codeforces/706/b.cpp
+++ codeforces/706/b.cpp
@@ -3,19 +3,21 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int binaryX(int *arr,int n,int ele){
-	int hi=n-1;
-	int lo=0;
-	int mid;
-	while(lo<hi){
-		mid=lo+((hi-lo)/2);
-		if(arr[mid]==ele) return mid;
-		else if(arr[mid]>ele){
-			hi=mid;
-		}
-		else lo=mid;
+int binaryX(int AR[], int N, int VAL){
+	int Mid,Lbound=0,Ubound=N-1;
+	while(Lbound<=Ubound)
+	{
+		Mid=(Lbound+Ubound)/2;
+		if(VAL>AR[Mid] && VAL<AR[Mid]) return ++Mid;
+		if(VAL<AR[Mid] && VAL>AR[Mid-1]) return Mid;
+		if(VAL>AR[Mid])
+			Lbound=Mid+1;
+		else if(VAL<AR[Mid])
+			Ubound=Mid-1;
+		else
+			return ++Mid;
 	}
-	return mid;
+	return false;
 }
 /*int linX(int *arr,int n,long long int ele){
 	int x=n;
