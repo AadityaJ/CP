ca1220c02a9ec277fa5867d8dbd209f70fd77a65
AadityaJ <aadityajamuar@gmail.com>
2017-03-01 23:52:10 +0530

    adding

--- codeforces/672/a.cpp
+++ codeforces/672/a.cpp
@@ -4,6 +4,17 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		int n,lim;
+		cin>>n>>lim;
+		int arr[n];
+		int min=INT_MAX;
+		for(int i=0;i<n;i++) {cin>>arr[i];if(min>arr[i]) min=arr[i];}
+		int x=lim-min;
+		if(x<=0) cout<<0<<endl;
+		else cout<<x<<endl;
+	}
 	return 0;
 }
