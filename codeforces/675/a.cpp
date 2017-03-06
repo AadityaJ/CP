1e247c48a8894f4b2eb3b37bbfe9b1426c9c5e9d
AadityaJ <aadityajamuar@gmail.com>
2017-03-07 00:49:00 +0530

    adding

--- codeforces/675/a.cpp
+++ codeforces/675/a.cpp
@@ -4,6 +4,14 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int a,b,c;
+	cin>>a>>b>>c;
+	if(c==0){
+		if(a==b) cout<<"YES";
+		else cout<<"NO";
+		return 0;
+	}
+	if((b-a)%c==0) cout<<"YES";
+	else cout<<"NO";
 	return 0;
 }
