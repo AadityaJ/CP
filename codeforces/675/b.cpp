9be262576cbfc96f7c194e227ac0ef0793cefeb8
AadityaJ <aadityajamuar@gmail.com>
2017-03-07 19:22:40 +0530

    adding

--- codeforces/675/b.cpp
+++ codeforces/675/b.cpp
@@ -2,8 +2,23 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#define lli long long int
 using namespace std;
 int main(int argc, char const *argv[]) {
+	int n, a, b, c, d;
+	cin>>n>>a>>b>>c>>d;
 
+	lli ans = 0;
+	for (int x = 1; x <= n; x++) {
+		int y = x + b - c;
+		int z = x + a - d;
+		int w = a + y - d;
+		if (1 <= y && y <= n && 1 <= z && z <= n && 1 <= w && w <= n) {
+			ans++;
+		}
+	}
+	ans *= n;
+
+	cout<<ans<<endl;
 	return 0;
 }
