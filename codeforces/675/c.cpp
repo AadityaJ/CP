187bbf04a695a8a00173695f4502755503085312
AadityaJ <aadityajamuar@gmail.com>
2017-03-07 19:37:33 +0530

    adding

--- codeforces/675/c.cpp
+++ codeforces/675/c.cpp
@@ -2,8 +2,21 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int n;
+	cin >> n;
+	map<long long, int> d;
+	long long sum = 0;
+	int ans = n - 1;
+	    for (int i = 0; i < n; i++) {
+	    int t;
+	    cin >> t;
+	    sum += t;
+	    d[sum]++;
+	    ans = min(ans, n - d[sum]);
+	}
+	cout << ans << endl;
 	return 0;
 }
