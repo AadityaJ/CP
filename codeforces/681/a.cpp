003dc87b7693f86399acfd3bcecfcc492466976a
AadityaJ <aadityajamuar@gmail.com>
2017-03-10 18:56:31 +0530

    adding

--- codeforces/681/a.cpp
+++ codeforces/681/a.cpp
@@ -4,6 +4,16 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int n;
+	bool isit=0;
+	cin>>n;
+	while(n--){
+		string str;
+		int n1,n2;
+		cin>>str;
+		cin>>n1>>n2;
+		if(n1>=2400 && n2>n1&&!isit){cout<<"YES";isit=1;}
+	}
+	if(!isit) cout<<"NO";
 	return 0;
 }
