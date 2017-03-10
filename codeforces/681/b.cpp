da775661b2cda60868481d1098a24f0e45c5603e
AadityaJ <aadityajamuar@gmail.com>
2017-03-10 19:25:55 +0530

    adding

--- codeforces/681/b.cpp
+++ codeforces/681/b.cpp
@@ -4,6 +4,21 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	long long int val;
+	cin>>val;
+	bool isit=0;
+	for(int i=0;i<=820;i++){
+		for(int j=0;j<=8100;j++){
+			if((val-(1234567*i+123456*j))%1234==0){isit=1;break;}
+		}
+	}
+	/*for(int i=0;i<=val;i+=1234567){
+		for(int j=0;j<=val-i;j+=123456){
+			if((val-(i+j))%1234==0) {isit=1;break;}
+			}
+		}
+	*/
+	if(isit)cout<<"YES";
+	else cout<<"NO";
 	return 0;
 }
