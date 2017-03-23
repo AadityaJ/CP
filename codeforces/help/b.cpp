3c9177c94bc86d4b3f70c58d0bdbbe4b1e630df8
AadityaJ <aadityajamuar@gmail.com>
2017-03-23 20:36:14 +0530

    adding

--- codeforces/help/b.cpp
+++ codeforces/help/b.cpp
@@ -3,7 +3,22 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int poly(string s){
+	if(s=="Tetrahedron") return 4;
+	if(s=="Cube") return 6;
+	if(s=="Octahedron") return 8;
+	if(s=="Dodecahedron") return 12;
+	if(s=="Icosahedron") return 20;
+}
 int main(int argc, char const *argv[]) {
-
+	int n;
+	cin>>n;
+	string s;
+	int cnt=0;
+	for(int i=0;i<n;i++){
+		cin>>s;
+		cnt+=poly(s);
+	}
+	cout<<cnt;
 	return 0;
 }
