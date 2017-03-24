4ee82ab928704fd5e5eb3b3fb624e2b6d8675997
AadityaJ <aadityajamuar@gmail.com>
2017-03-24 12:23:08 +0530

    adding

--- codeforces/787/a.cpp
+++ codeforces/787/a.cpp
@@ -8,12 +8,11 @@ bool isIn(int b,int a,int x){
 }
 bool isEven(int a){return a%2==0;}
 int main(int argc, char const *argv[]) {
+	int n=100;
 	int a,b,c,d;
 	cin>>a>>b;
 	cin>>c>>d;
-	if(isEven(a)&&!isEven(b)&&isEven(c)&&isEven(d)) {cout<<-1;return 0;}
-	if(isEven(a)&&isEven(b)&&isEven(c)&&!isEven(d)) {cout<<-1;return 0;}
-	for(int x=0;;x++){
+	for(int x=max(b,d);x<=(n*n);x++){
 		if(isIn(b,a,x)&&isIn(d,c,x)){
 			cout<<x;return 0;
 		}
