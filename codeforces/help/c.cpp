7c7617983d2d979bea3efe1cf959e0fbfcce7cc8
AadityaJ <aadityajamuar@gmail.com>
2017-03-24 11:55:30 +0530

    adding

--- codeforces/help/c.cpp
+++ codeforces/help/c.cpp
@@ -4,6 +4,17 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		int n,k;
+		string s;
+		cin>>n>>k;
+		cin>>s;
+		string str="";
+		for(int i=0;i<s.length();i++){
+			str+=()
+		}
+	}
 	return 0;
 }
