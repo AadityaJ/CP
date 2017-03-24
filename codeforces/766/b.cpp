19a5f0eaf7a166187be583b3f3fd6731556a2278
AadityaJ <aadityajamuar@gmail.com>
2017-03-25 00:24:17 +0530

    adding

--- codeforces/766/b.cpp
+++ codeforces/766/b.cpp
@@ -4,6 +4,16 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	sort(arr,arr+n);
+	for(int i=n-1;i>=2;i--){
+		if(arr[i]<arr[i-1]+arr[i-2]){cout<<"YES";return 0;}
+	}
+	cout<<"NO";
 	return 0;
 }
