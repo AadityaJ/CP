c231f3d33eab1230cc8138abafd8fbd1029c0d5e
AadityaJ <aadityajamuar@gmail.com>
2017-03-25 00:13:01 +0530

    adding

--- codeforces/766/a.cpp
+++ codeforces/766/a.cpp
@@ -4,6 +4,9 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	string s1,s2;
+	cin>>s1>>s2;
+	if(s1==s2){cout<<-1;}
+	else cout<<max(s1.length(),s2.length());
 	return 0;
 }
