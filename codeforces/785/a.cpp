700539d58e43a8f869e7532173a25a4b603a90f6
AadityaJ <aadityajamuar@gmail.com>
2017-03-27 22:45:04 +0530

    adding

--- codeforces/785/a.cpp
+++ codeforces/785/a.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 bool compare(const pair<int, int>&i, const pair<int, int>&j)
 {
-    return i.second < j.second;
+    return i.first < j.first;
 }
 int main(int argc, char const *argv[]) {
 	int n;
@@ -15,11 +15,15 @@ int main(int argc, char const *argv[]) {
 	pair<int,int> p_n[MAX];
 	for(int i=0;i<n;i++) cin>>p_n[i].first>>p_n[i].second;
 	sort(p_n,p_n+n,compare);
-	for(int i=0;i<n;i++) cout<<p_n[i].first<<p_n[i].second;
+	//for(int i=0;i<n;i++) cout<<p_n[i].first<<p_n[i].second;
 	int m;
 	cin>>m;
 	pair<int,int> p_m[MAX];
 	for(int i=0;i<n;i++) cin>>p_m[i].first>>p_m[i].second;
-
+	sort(p_m,p_m+m,compare);
+	//for(int i=0;i<n;i++) cout<<p_m[i].first<<p_m[i].second;
+	int min_r=INT_MAX,min_l=INT_MAX;
+	int max_r=INT_MIN,max_l=INT_MIN;
+	
 	return 0;
 }
