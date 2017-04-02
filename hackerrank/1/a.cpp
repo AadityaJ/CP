57b3d0e9ea311561f40bb53f925f825f9375fc00
AadityaJ <aadityajamuar@gmail.com>
2017-04-02 23:01:57 +0530

    adding

--- hackerrank/1/a.cpp
+++ hackerrank/1/a.cpp
@@ -4,6 +4,19 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
-	return 0;
+	int n;
+    cin >> n;
+    vector<int> file(n);
+    for(int file_i = 0; file_i < n; file_i++){
+       cin >> file[file_i];
+    }
+    int cnt=0,i=0;
+	while(i<n){
+		int v=file[i];
+		i+=(v+1);
+		cnt++;
+	}
+	//if(i==n) cnt--;
+	cout<<cnt;
+    return 0;
 }
--- hackerrank/1/b.cpp
+++ hackerrank/1/b.cpp
@@ -3,7 +3,46 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int main(int argc, char const *argv[]) {
+int dp_val[5000][5000];
+int cnt;
+int xor_val(std::vector<int> s,int i,int j,int n){
+	int val=0;
+	for(int x=1;x<i;x++){
+		val^=s[x];
+	}
+	for(int x=j+1;x<=n;x++){
+		val^=s[x];
+	}
+	return val;
+}
+int dp(std::vector<int> s,int n){
+	int val=0;
+	for(int x=1;x<=n;x++){
+		dp_val[1][x]=xor_val(s,1,x,n);
+		if(dp_val[1][x]==0) cnt++;
+	}
+	for(int x=2;x<=n;x++){
+		for(int y=1;y<=n;y++){
+			dp_val[x][y]=dp_val[x-1][y]^(s[x]);
+			if(dp_val[x][y]==0) cnt++;
+		}
+	}
+}
 
-	return 0;
+int main(int argc, char const *argv[]) {
+	int n;
+    cin >> n;
+    vector<int> s(n+1);
+    for(int s_i = 1; s_i <=n; s_i++){
+       cin >> s[s_i];
+    }
+	dp(s,n);
+    /*int arr[n+1][n+1];
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			arr[i][j]=-100;
+		}
+	}*/
+	cout<<cnt;
+    return 0;
 }
