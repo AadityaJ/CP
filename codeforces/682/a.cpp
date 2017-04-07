f702c4e31d48787cb63a83222d967eaabc1dc0ce
AadityaJ <aadityajamuar@gmail.com>
2017-04-07 21:15:07 +0530

    adding

--- codeforces/682/a.cpp
+++ codeforces/682/a.cpp
@@ -6,9 +6,11 @@ using namespace std;
 int main(int argc, char const *argv[]) {
 	long long n,m;
 	cin>>n>>m;
+	int a=max(n,m);
+	int b=n+m-a;
 	int x=0;
-	for(int i=1;i<=n;i++){
-		x+=(m+i)/5;
+	for(int i=1;i<=b;i++){
+		x+=((a+i)/5)-(i/5);
 	}
 	cout<<x;
 	return 0;
--- codeforces/706/c.cpp
+++ codeforces/706/c.cpp
@@ -4,6 +4,6 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-	
+
 	return 0;
 }
