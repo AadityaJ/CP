8103672d3732e072266cf34443daa072103fa88e
AadityaJ <aadityajamuar@gmail.com>
2017-06-04 15:48:43 +0530

    adding

--- codeforces/g3/b.cpp
+++ codeforces/g3/b.cpp
@@ -4,6 +4,16 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int n,m;
+	cin>>n>>m;
+	int arr[n+1];
+	for(int i=1;i<=n;i++) cin>>arr[i];
+	int sum=0;
+	for(int i=0;i<m;i++){
+		int x,y;
+		cin>>x>>y;
+		sum+=min(arr[x],arr[y]);
+	}
+	cout<<sum<<endl;
 	return 0;
 }
