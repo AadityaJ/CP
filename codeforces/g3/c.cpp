4bb41097aa0b4607363fad456956e3e4a2d179d0
AadityaJ <aadityajamuar@gmail.com>
2017-06-04 15:56:06 +0530

    adding

--- codeforces/g3/c.cpp
+++ codeforces/g3/c.cpp
@@ -2,8 +2,36 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#define MX 101
 using namespace std;
+std::vector<int> G[MX];
+bool visited[MX];
+void dfs(int i){
+	visited[i]=1;
+	for(int x=0;x<G[i].size();x++){
+		if(!visited[G[i][x]]) dfs(G[i][x]);
+	}
+}
+bool conn(int n){
+	dfs(1);
+	for(int i=1;i<=n;i++){
+		if(!visited[i]) return 0;
+	}
+	return 1;
+}
 int main(int argc, char const *argv[]) {
-
+	int n,m;
+	cin>>n>>m;
+	for(int i=0;i<m;i++){
+		int x,y;
+		cin>>x>>y;
+		G[x].push_back(y);
+		G[y].push_back(x);
+	}
+	if(n!=m) cout<<"NO\n";
+	else{
+		if(conn(n)) cout<<"FHTAGN!\n";
+		else cout<<"NO\n";
+	}
 	return 0;
 }
