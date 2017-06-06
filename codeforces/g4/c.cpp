af8267ffec004d7c2d24d58a92629bcb6567cc76
AadityaJ <aadityajamuar@gmail.com>
2017-06-06 20:39:54 +0530

    adding

--- codeforces/g4/c.cpp
+++ codeforces/g4/c.cpp
@@ -4,6 +4,20 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		int n,p;
+		cin>>n>>p;
+		int a=1,b=2;
+		for(int i=0;i<(2*n+p);i++){
+			cout<<a<<" "<<b<<endl;
+			b++;
+			if(b>n){
+				a++;
+				b=a+1;
+			}
+		}
+	}
 	return 0;
 }
