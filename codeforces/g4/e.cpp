a36b6a79daba2311db4b653dec476c53ce196d1e
AadityaJ <aadityajamuar@gmail.com>
2017-06-08 19:23:26 +0530

    adding

--- codeforces/g4/e.cpp
+++ codeforces/g4/e.cpp
@@ -4,6 +4,19 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int n,m;
+	cin>>n>>m;
+	int arr[n];
+	for(int i=0;i<n;i++) cin>>arr[i];
+	int pv=-1,hi=n-1,lo=0;
+	while(hi>lo){
+		int mid=(hi+lo)/2;
+		if(arr[mid]>arr[mid+1]){pv=mid;break;}
+		else if(arr[mid-1]>arr[mid]){pv=mid-1;break;}
+		else if(arr[mid]>arr[lo]){lo=mid+1;}
+		else {hi=mid-1;}
+	}
+	//cout<<pv<<endl;
+	
 	return 0;
 }
