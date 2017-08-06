0b7b111f1facc7bc34cfe4dbc966ec38f1680530
AadityaJ <aadityajamuar@gmail.com>
2017-08-06 16:29:47 +0530

    adding

--- codechef/aug17/2.cpp
+++ codechef/aug17/2.cpp
@@ -4,18 +4,20 @@
 #include <algorithm>
 #include <cmath>
 using namespace std;
-bool checkAllZ(int *v,int n){
+bool checkAllZ(long long int *v,int n){
 	for(int i=0;i<n;i++){
 		if(v[i]!=0) return 1;
 	}
 	return 0;
 }
-int sumC(int *v,int n){
-	int sm=0;
-	for(int i=0;i<n;i++){
-		sm+=v[i];
+int f(std::vector<int> &v){
+	long long int x=0;
+	for(int i=0;i<v.size()-1;i++){
+		v[i+1]+=v[i];
+		x+=abs(v[i]);
+		v[i]=0;
 	}
-	return sm;
+	return x;
 }
 int main(int argc, char const *argv[]) {
 	int t;
@@ -24,7 +26,7 @@ int main(int argc, char const *argv[]) {
 		int n,d;
 		cin>>n>>d;
 		int arr[n];
-		int sx=0;
+		long long int sx=0;
 		for(int i=0;i<n;i++) {cin>>arr[i];sx+=arr[i];}
 		if(sx%n){
 			cout<<"-1\n";
@@ -37,18 +39,28 @@ int main(int argc, char const *argv[]) {
 		for(int i=0;i<n;i++){
 			val[i]=(arr[i]-sx);
 		}
-		int v[d];
-		int posS[d];
+		long long int v[d];
+		vector<int> levl[d];
 		for(int i=0;i<d;i++){
 			v[i]=0;
-			posS[i]=0;
 		}
 		for(int i=0;i<n;i++){
 			v[i%d]+=val[i];
-			if(val[i]>0) posS[i%d]+=val[i];
+			levl[i%d].push_back(val[i]);
+		}
+		for(int i=0;i<d;i++){
+			cout<<i<<":: ";
+			for(int j=0;j<levl[i].size();j++) cout<<levl[i][j]<<" ";
+			cout<<endl;
 		}
 		if(checkAllZ(v,d)) cout<<"-1\n";
-		else cout<<sumC(posS,d)<<endl;
+		else{
+			long long int res=0;
+			for(int i=0;i<d;i++){
+				res+=f(levl[i]);
+			}
+			cout<<res<<endl;
+		}
 	}
 	 return 0;
 }
