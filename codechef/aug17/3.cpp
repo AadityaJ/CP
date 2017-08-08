2d6466d7506254ef2daaf610a8e967f3116b5a57
AadityaJ <aadityajamuar@gmail.com>
2017-08-08 23:54:11 +0530

    adding

--- codechef/aug17/3.cpp
+++ codechef/aug17/3.cpp
@@ -39,22 +39,37 @@ int main(int argc, char const *argv[]) {
 			cin>>offs[i]>>maxJob[i];
 		}
 		for(int i=0;i<n;i++){
-			for(int j=0;j<m;j++) cin>>qual[i][j];
+			string str;
+			cin>>str;
+			for(int j=0;j<str.length();j++){
+				qual[i][j]=(str[j]-'0');
+			}
 		}
-		int mx=0;
-		int index=0;
-		int sal=0;
+		long long int sal=0;
+		/*for(int i=0;i<n;i++) cout<<minSalary[i]<<" ";
+		cout<<endl;
+		for(int i=0;i<m;i++) cout<<offs[i]<<" ";
+		cout<<endl;
+		for(int i=0;i<m;i++) cout<<maxJob[i]<<" ";
+		cout<<endl;
 		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++) cout<<qual[i][j]<<' ';
+			cout<<endl;
+		}*/
+		for(int i=0;i<n;i++){
+			int mx=-1;
+			int index=0;
 			for(int j=0;j<m;j++){
-				if(qual[i][j]&&maxJob[m]>0&&minSalary[i]<=offs[j]){
+				if(qual[i][j]&&maxJob[j]>0&&minSalary[i]<=offs[j]){
 					if(mx<offs[j]){
 						mx=offs[j];
 						index=j;
 					}
 				}
 			}
-			if(mx==0) {continue;}
+			if(mx==-1) {continue;}
 			sal+=(mx);
+			//cout<<i<<" "<<index<<endl;
 			emp[index]=1;
 			pla[i]=1;
 			maxJob[index]--;
