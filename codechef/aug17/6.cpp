2cdf33ef5df5cd0b2a5456134f010678f72dc6fd
AadityaJ <aadityajamuar@gmail.com>
2017-08-11 17:27:47 +0530

    adding

--- codechef/aug17/6.cpp
+++ codechef/aug17/6.cpp
@@ -3,6 +3,17 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+long long int f(vector<string> &v,string str){
+    for(int i=0;i<str.length();i++){
+        bool is=0;
+        char c=str[i];
+        for(int j=0;j<v.size();j++){
+            if(v[j][i]==c){is=1;break;}
+        }
+        if(!is){v.push_back(str);return str.length()-i;}
+    }
+    return 0;
+}
 string con2bits(int c,int n){
     string str="";
     while(c){
@@ -15,6 +26,19 @@ string con2bits(int c,int n){
     reverse(str.begin(),str.end());
     return str;
 }
+string add(string a,string b){
+    int ca=0;
+    string c="";
+    for(int i=0;i<a.length();i++){
+        int ax=a[i];
+        int bx=b[i];
+        int cx=(ax+bx+ca)%2;
+        ca=(ax+bx+ca)/2;
+        c.push_back(cx+'0');
+    }
+    reverse(c.begin(),c.end());
+    return c;
+}
 int main(int argc, char const *argv[]) {
 	int t;
 	cin>>t;
@@ -23,6 +47,7 @@ int main(int argc, char const *argv[]) {
         cin>>n>>q;
         long long int ans=0;
         string x="";
+        for(int i=0;i<n;i++) x.push_back('0');
         vector<string> v;
         for(int i=0;i<q;i++){
             char ch;
@@ -32,9 +57,10 @@ int main(int argc, char const *argv[]) {
                 int c;
                 cin>>c;
                 string str=con2bits(c,n);
-                cout<<str<<endl;
-                //x=add(x,str);
-                //ans+=f(str,v);
+                //cout<<str<<endl;
+                x=add(x,str);
+                //cout<<x<<endl;
+                ans+=f(v,str);
             }
         }
 	}
