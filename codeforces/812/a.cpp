04109ef04c7bc6a1869d61dead4e4bd828f16b22
AadityaJ <aadityajamuar@gmail.com>
2017-09-01 01:14:44 +0530

    adding

---
+++ codeforces/812/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(int argc, char const *argv[]) {
+	bool mp[5][5];
+	for(int i=1;i<=4;i++){
+		for(int j=1;j<=4;j++) cin>>mp[i][j];
+	}
+	//if((mp[1][1]==1)
+	return 0;
+}
