bd85b412e34456a6d5dfe8c02cdcf2ca998c90c4
AadityaJ <aadityajamuar@gmail.com>
2017-09-13 18:39:14 +0530

    adding

--- codeforces/832/c.cpp
+++ codeforces/832/c.cpp
@@ -4,6 +4,14 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int t;
+	cin>>t;
+	while(t--){
+		int a,b,c;
+		cin>>a>>b>>c;
+		if(a<b && a<c){cout<<"First\n";}
+		if(b<a && b<c){cout<<"Second\n";}
+		if(c<a && c<b){cout<<"Third\n";}
+	}
 	return 0;
 }
