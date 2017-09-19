04f38e00ff15f1e64e683c5308d53a4ce2844864
AadityaJ <aadityajamuar@gmail.com>
2017-09-20 00:14:35 +0530

    adding

--- codeforces/832/a.cpp
+++ codeforces/832/a.cpp
@@ -4,8 +4,15 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-	long long int n,k;
-	cin>>n>>k;
-	((n/k)%2)?(cout<<"YES"):(cout<<"NO");
+		int n;
+		cin>>n;
+		int num_e=0;
+		for(int i=0;i<n;i++){
+			int x;
+			cin>>x;
+			if(x%2==0) num_e++;
+		}
+		if(num_e>(n/2)) cout<<"READY FOR BATTLE\n";
+		else cout<<"NOT READY\n";
 	return 0;
 }
