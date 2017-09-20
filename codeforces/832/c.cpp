be839838278f81c753b0c4fddfa9bdd3e2d29480
AadityaJ <aadityajamuar@gmail.com>
2017-09-20 10:40:14 +0530

    adding

--- codeforces/832/c.cpp
+++ codeforces/832/c.cpp
@@ -3,15 +3,32 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int mod(int a,int b){
+	int x=(a%b);
+	if(x==0) return b;
+	return x;
+}
+bool check(int *arr,int n,int k){
+	for(int i=1;i<=n;i++){
+		if(abs(arr[i]-i)<k) return 0;
+	}
+	return 1;
+}
 int main(int argc, char const *argv[]) {
 	int t;
 	cin>>t;
 	while(t--){
-		int a,b,c;
-		cin>>a>>b>>c;
-		if(a<b && a<c){cout<<"First\n";}
-		if(b<a && b<c){cout<<"Second\n";}
-		if(c<a && c<b){cout<<"Third\n";}
+		int n,k;
+		cin>>n>>k;
+		int arr[n+1];
+		for(int i=1;i<=n;i++){
+			arr[i]=mod(k+i,n);
+		}
+		if(!check(arr,n,k)) cout<<"-1\n";
+		else{
+			for(int i=1;i<=n;i++) cout<<arr[i]<<" ";
+			cout<<endl;
+		}
 	}
 	return 0;
 }
