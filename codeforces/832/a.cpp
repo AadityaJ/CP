6b1f1ac9e20dba420a2107b6c0f48a046f5240b0
AadityaJ <aadityajamuar@gmail.com>
2017-09-22 21:51:34 +0530

    adding

--- codeforces/832/a.cpp
+++ codeforces/832/a.cpp
@@ -11,20 +11,30 @@ void init(){
 	}
 }
 
-int f(int i,int j,int n,int m){
-	if(i>=n || j>=m) return 0;
+int f(int n,int m){
+	/*if(i>=n || j>=m) return 0;
 	if(i==n-1 && j==m-1) return 1;
 	if(dp[i][j]!=-1) return dp[i][j];
-	return dp[i][j]=(f(i+1,j,n,m)+f(i,j+1,n,m))%MOD;
+	return dp[i][j]=(f(i+1,j,n,m)+f(i,j+1,n,m))%MOD;*/
+	int dp[n][m];
+	for(int i=0;i<n;i++) dp[i][0]=1;
+	for(int i=0;i<m;i++) dp[0][i]=1;
+	for(int i=1;i<n;i++){
+		for(int j=1;j<m;j++){
+			dp[i][j]=(dp[i-1][j]+dp[i][j-1])%MOD;
+		}
+	}
+	return dp[n-1][m-1];
+
 }
 int main(){
 	int t;
 	cin>>t;
 	while(t--){
-		init();
+		//init();
 		int n,m;
 		cin>>n>>m;
-		cout<<f(0,0,n,m)<<endl;
+		cout<<f(n,m)<<endl;
 	}
 	return 0;
 }
