8e64392bbca21ab709e8d347ec1d7408fee88ecd
AadityaJ <aadityajamuar@gmail.com>
2017-10-01 12:24:25 +0530

    adding

--- codeforces/841/a.cpp
+++ codeforces/841/a.cpp
@@ -4,6 +4,19 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	int n,m;
+	cin>>n>>m;
+	string str;
+	cin>>str;
+	int mp[26]={0};
+	for(int i=0;i<str.length();i++){
+		mp[str[i]-'a']++;
+	}
+	bool is=1;
+	for(int i=0;i<26;i++){
+		if(mp[i]>m){is=0;break;}
+	}
+	if(is) cout<<"YES\n";
+	else cout<<"NO\n";
 	return 0;
 }
