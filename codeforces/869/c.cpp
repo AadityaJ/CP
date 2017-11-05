6b59fa312371aa65fa55bed7e2d87560dc2f3157
AadityaJ <aadityajamuar@gmail.com>
2017-11-05 15:33:12 +0530

    adding

--- codeforces/869/c.cpp
+++ codeforces/869/c.cpp
@@ -4,6 +4,15 @@
 #include <algorithm>
 using namespace std;
 int main(int argc, char const *argv[]) {
-
+	string str;
+	cin>>str;
+	if(str.length()<6) cout<<"no\n";
+	else{
+		int num_1=0;
+		for(int i=0;i<6;i++){
+			if(str[str.length()-1-i]=='1') num_1++;
+		}
+		
+	}
 	return 0;
 }
