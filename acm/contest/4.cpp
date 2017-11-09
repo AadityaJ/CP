8d12559c240924bd0d8dd59f2f323efd414bfcd3
AadityaJ <aadityajamuar@gmail.com>
2017-11-10 01:07:28 +0530

    adding

--- acm/contest/4.cpp
+++ acm/contest/4.cpp
@@ -7,7 +7,20 @@ int main(int argc, char const *argv[]){
 	int t;
 	cin>>t;
 	while(t--){
-
+		int n;
+		cin>>n;
+		int a[n],b[n];
+		for(int i=0;i<n;i++) cin>>a[i];
+		for(int i=0;i<n;i++) {cin>>b[i];a[i]%=b[i];}
+		long long int ans=0,mx=0;
+		for(int i=0;i<n;i++){
+			if(a[i]+b[i]>mx){mx=(a[i]+b[i]);}
+		}
+		//cout<<mx<<" ";
+		for(int i=0;i<n;i++){
+			ans=((mx-a[i])%b[i])+ans;
+		}
+		cout<<ans<<endl;
 	}
 	return 0;
 }
