d33d17153cb2a958a92b8f19a8d16a6c91306723
AadityaJ <aadityajamuar@gmail.com>
2017-12-05 22:52:00 +0530

    adding

--- acm/contest/5.cpp
+++ acm/contest/5.cpp
@@ -3,11 +3,26 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int main(int argc, char const *argv[]){
-	int t;
-	cin>>t;
-	while(t--){
-
+bool checkifPalin(string str){
+	for(int i=0;i<str.length();i++){
+		if(str[i]!=str[str.length()-i-1]){return 0;}
 	}
+	return 1;
+}
+int main(int argc, char const *argv[]){
+	string str;
+	cin>>str;
+	bool is=1;
+	for(int i=0;i<str.length();i++){
+		string temp="";
+		for(int j=i;j<str.length();j++){
+			temp+=str[j];
+			bool ch=checkifPalin(temp);
+			if(ch && (temp.length()%2==0)){is=0;break;}
+		}
+	}
+	//cout<<is<<endl;
+	if(is){cout<<"Odd .\n";}
+	else cout<<"Or not .\n";
 	return 0;
 }
--- acm/contest/6.cpp
+++ acm/contest/6.cpp
@@ -2,12 +2,14 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 int main(int argc, char const *argv[]){
-	int t;
-	cin>>t;
-	while(t--){
-
-	}
+	int k,p,x;
+	cin>>k>>p>>x;
+	double m_=sqrt((k*p*1.00)/(x*1.00));
+	double m=(int)m_+1;
+	cout<<setprecision(5)<<((k*p*1.00)/m)+(m*x*1.00)<<endl;
 	return 0;
 }
