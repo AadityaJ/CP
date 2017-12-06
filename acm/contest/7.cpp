0ac8da20fea6fe12c16d8d726166de32a2bfa93d
AadityaJ <aadityajamuar@gmail.com>
2017-12-06 20:39:05 +0530

    adding

--- acm/contest/7.cpp
+++ acm/contest/7.cpp
@@ -2,12 +2,22 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
 using namespace std;
 int main(int argc, char const *argv[]){
-	int t;
-	cin>>t;
-	while(t--){
-
+	int a,b;
+	cin>>a>>b;
+	map<int,int> mp;
+	for(int i=1;i<=b;i++){
+		for(int j=(a/i)*i;j<=b;j+=i){
+			mp[j]+=i;
+		}
 	}
+	long long int sum=0;
+	for(int i=a;i<=b;i++){
+		//cout<<i<<endl;
+		sum+=mp[i];
+	}
+	cout<<sum<<endl;
 	return 0;
 }
--- acm/contest/8.cpp
+++ acm/contest/8.cpp
@@ -2,12 +2,25 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <climits>
 using namespace std;
 int main(int argc, char const *argv[]){
-	int t;
-	cin>>t;
-	while(t--){
-
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++) cin>>arr[i];
+	int ans=INT_MAX;
+	int sum=0;
+	int l=0;
+	for(int i=0;i<n;i++){
+		sum+=arr[i];
+		while(sum>=180){
+			ans=min(ans,2*abs(180-sum));
+			sum-=arr[l];
+			l++;
+		}
+		ans=min(ans,2*abs(180-sum));
 	}
+	cout<<ans<<endl;
 	return 0;
 }
